b081d3f4d4d1711c610eb7470a6922bd49821cbd
Vaults <marcel1337@hotmail.com>
2014-02-13 11:21:52 +0100

    Added Company module

--- config/application.config.php
+++ config/application.config.php
@@ -7,6 +7,7 @@ return array(
         'DoctrineORMModule',
         'User',
         'Application',
+		'Company',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
