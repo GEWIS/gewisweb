9a985a571bfec72729400e2e45fd0eb6ebf9bfe7
Vaults <marcel1337@hotmail.com>
2014-02-13 11:21:52 +0100

    Added Company module

---
+++ module/Company/config/module.config.php
@@ -0,0 +1,54 @@
+<?php
+return array(
+    'router' => array(
+        'routes' => array(
+            'user' => array(
+                'type'    => 'Literal',
+                'options' => array(
+                    'route'    => '/company',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'Company\Controller',
+                        'controller'    => 'Company',
+                        'action'        => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'default' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '[/:action]',
+                            'constraints' => array(
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'controllers' => array(
+        'invokables' => array(
+            'Company\Controller\Company' => 'Company\Controller\CompanyController'
+        )
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'company' => __DIR__ . '/../view/'
+        )
+    ),
+    'doctrine' => array(
+        'driver' => array(
+            'company_entities' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(__DIR__ . '/../src/Company/Model/')
+            ),
+            'orm_default' => array(
+                'drivers' => array(
+                    'Company\Model' => 'company_entities'
+                )
+            )
+        )
+    )
+);
