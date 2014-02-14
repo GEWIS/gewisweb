78318a643f33bc69ae4aaad7476bfd0da99e4c09
Pieter Kokx <pieter@kokx.nl>
2014-02-14 17:51:24 +0100

    Added member model.

---
+++ module/Decision/src/Decision/Model/Member.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace Decision\Model;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Member model.
+ *
+ * @ORM\Entity
+ */
+class Member
+{
+
+    /**
+     * Membership number
+     *
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     */
+    protected $lidnr;
+
+    /**
+     * Member's email address.
+     *
+     * @ORM\Column(type="string")
+     */
+    protected $email;
+
+    /**
+     * Member's last name.
+     *
+     * @ORM\Column(type="string")
+     */
+    protected $lastName;
+
+    /**
+     * Middle name.
+     *
+     * @ORM\Column(type="string")
+     */
+    protected $middleName;
+
+    /**
+     * Initials.
+     *
+     * @ORM\Column(type="string")
+     */
+    protected $initials;
+
+    /**
+     * First name.
+     *
+     * @ORM\Column(type="string")
+     */
+    protected $firstName;
+
+    // TODO: Much, MUCH more fields
+
+    /**
+     * Get the membership number.
+     *
+     * @return int
+     */
+    public function getLidnr()
+    {
+        return $this->lidnr;
+    }
+
+    /**
+     * Get the member's email address.
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Get the member's last name.
+     *
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Get the member's middle name.
+     *
+     * @return string
+     */
+    public function getMiddleName()
+    {
+        return $this->middleName;
+    }
+
+    /**
+     * Get the member's initials.
+     *
+     * @return string
+     */
+    public function getInitials()
+    {
+        return $this->initials;
+    }
+
+    /**
+     * Get the member's first name.
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set the member's email address.
+     *
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * Set the member's last name.
+     *
+     * @param string $lastName
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
+
+    /**
+     * Set the member's middle name.
+     *
+     * @param string $middleName
+     */
+    public function setMiddleName($middleName)
+    {
+        $this->middleName = $middleName;
+    }
+
+    /**
+     * Set the member's initials.
+     *
+     * @param string $initals
+     */
+    public function setInitials($initials)
+    {
+        $this->initials = $initials;
+    }
+
+    /**
+     * Set the member's first name.
+     *
+     * @param string $firstName
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+}
