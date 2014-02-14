a8d0c1f5d299a11ef3eb596febb0b921708254e4
Pieter Kokx <pieter@kokx.nl>
2014-02-14 15:38:40 +0100

    Added Decision and Meeting database module.

---
+++ module/Decision/config/module.config.php
@@ -0,0 +1,54 @@
+<?php
+return array(
+    'router' => array(
+        'routes' => array(
+            'decision' => array(
+                'type'    => 'Literal',
+                'options' => array(
+                    'route'    => '/decision',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'Decision\Controller',
+                        'controller'    => 'Decision',
+                        'action'        => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'default' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '[/:action]',
+                            'constraints' => array(
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'controllers' => array(
+        'invokables' => array(
+            'Decision\Controller\Decision' => 'Decision\Controller\DecisionController'
+        )
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'decision' => __DIR__ . '/../view/'
+        )
+    ),
+    'doctrine' => array(
+        'driver' => array(
+            'decision_entities' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(__DIR__ . '/../src/Decision/Model/')
+            ),
+            'orm_default' => array(
+                'drivers' => array(
+                    'Decision\Model' => 'decision_entities'
+                )
+            )
+        )
+    )
+);
