3188c61b62b048afda026308ed565950d4ece9b8
Pieter Kokx <pieter@kokx.nl>
2014-02-15 11:01:25 +0100

    Added basic message creation.

--- module/User/src/User/Service/Email.php
+++ module/User/src/User/Service/Email.php
@@ -8,6 +8,8 @@ use Decision\Model\Member as MemberModel;
 
 use Zend\ServiceManager\ServiceManager;
 use Zend\ServiceManager\ServiceManagerAwareInterface;
+use Zend\Mail\Message;
+use Zend\View\Model\ViewModel;
 
 class Email implements ServiceManagerAwareInterface
 {
@@ -28,7 +30,58 @@ class Email implements ServiceManagerAwareInterface
      */
     public function sendRegisterEmail(NewUserModel $newUser, MemberModel $member)
     {
-        // TODO: generate and send the email
+        $body = $this->render('user/email/register', array(
+            'user' => $newUser,
+            'member' => $member
+        ));
+
+        $message = new Message();
+
+        // TODO: configuration for this
+        $message->addFrom('web@gewis.nl');
+        $message->addTo($newUser->getEmail());
+        $message->setBody($body);
+
+        $this->send($message);
+    }
+
+    /**
+     * Send a message.
+     *
+     * @param Message $message
+     */
+    public function send(Message $message)
+    {
+        // TODO: send the message
+        var_dump($message);
+    }
+
+    /**
+     * Render a template with given variables.
+     *
+     * @param string $template
+     * @param array $vars
+     *
+     * @return string/
+     */
+    public function render($template, $vars)
+    {
+        $renderer = $this->getRenderer();
+
+        $model = new ViewModel($vars);
+        $model->setTemplate($template);
+
+        return $renderer->render($model);
+    }
+
+    /**
+     * Get the renderer for the email.
+     *
+     * @return PhpRenderer
+     */
+    public function getRenderer()
+    {
+        return $this->sm->geT('view_manager')->getRenderer();
     }
 
     /**
