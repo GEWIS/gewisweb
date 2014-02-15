482cd4065e63faa125901705ed5e3c944ddae690
Pieter Kokx <pieter@kokx.nl>
2014-02-15 10:23:10 +0100

    Added cost configuration for bcrypt.

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,5 +12,19 @@
  */
 
 return array(
-    // ...
+    /**
+     * Bcrypt cost.
+     *
+     * DO NOT CHANGE THE PASSWORD HASH SETTINGS FROM THEIR DEFAULTS
+     * Unless A) you have done sufficient research and fully understand exactly
+     * what you are changing, AND B) you have a very specific reason to deviate
+     * from the default settings and know what you're doing.
+     *
+     * The password hash settings may be changed at any time without
+     * invalidating existing user accounts.
+     *
+     * The number represents the base-2 logarithm of the iteration count used for
+     * hashing. Default is 14 (about 10 hashes per second on an i5).
+     */
+    'bcrypt_cost' => 14
 );
