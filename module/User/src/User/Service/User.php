f25f60ed27f0a7e7dd7fe1eed0bf02a511ac54a9
Pieter Kokx <pieter@kokx.nl>
2014-02-16 17:58:17 +0100

    Added error messages to member registration.

--- module/User/src/User/Service/User.php
+++ module/User/src/User/Service/User.php
@@ -6,6 +6,8 @@ use User\Model\User as UserModel;
 use User\Model\NewUser as NewUserModel;
 use User\Mapper\User as UserMapper;
 
+use User\Form\Register as RegisterForm;
+
 use Decision\Model\Member as MemberModel;
 
 use Zend\ServiceManager\ServiceManager,
@@ -77,16 +79,21 @@ class User implements ServiceManagerAwareInterface
         $data = $form->getData();
         $member = $this->getMemberMapper()->findByLidnr($data['lidnr']);
 
+        if (null === $member) {
+            $form->setError(RegisterForm::ERROR_MEMBER_NOT_EXISTS);
+            return null;
+        }
+
         // check if the email is the same
         if ($member->getEmail() != $data['email']) {
-            // TODO: error, wrong email address
+            $form->setError(RegisterForm::ERROR_WRONG_EMAIL);
             return null;
         }
 
         // check if the member already has a corresponding user.
         $user = $this->getUserMapper()->findByLidnr($member->getLidnr());
         if (null !== $user) {
-            // TODO: error, user already exists
+            $form->setError(RegisterForm::ERROR_USER_ALREADY_EXISTS);
             return null;
         }
 
