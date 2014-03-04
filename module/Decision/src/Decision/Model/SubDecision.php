bcff0785f3bc5dd46c4a28b55ea95a4a8bfecaf4
Pieter Kokx <pieter@kokx.nl>
2014-03-04 18:48:43 +0100

    Added subdecision setters.

--- module/Decision/src/Decision/Model/SubDecision.php
+++ module/Decision/src/Decision/Model/SubDecision.php
@@ -154,4 +154,233 @@ class SubDecision
      * @ORM\Column(type="string", nullable=true)
      */
     protected $type;
+
+
+    /**
+     * Get the decision.
+     *
+     * @return Decision
+     */
+    public function getDecision()
+    {
+        return $this->decision;
+    }
+
+    /**
+     * Set the decision.
+     *
+     * @param Decision $decision
+     */
+    public function setDecision(Decision $decision)
+    {
+        $this->meeting_type = $decision->getMeetingType();
+        $this->meeting_number = $decision->getMeetingNumber();
+        $this->decision_point = $decision->getPoint();
+        $this->decision_number = $decision->getNumber();
+        $this->decision = $decision;
+    }
+
+    /**
+     * Get the meeting type.
+     *
+     * @return string
+     */
+    public function getMeetingType()
+    {
+        return $this->meeting_type;
+    }
+
+    /**
+     * Get the meeting number.
+     *
+     * @return int
+     */
+    public function getMeetingNumber()
+    {
+        return $this->meeting_number;
+    }
+
+    /**
+     * Get the decision point number.
+     *
+     * @return int
+     */
+    public function getDecisionPoint()
+    {
+        return $this->decision_point;
+    }
+
+    /**
+     * Get the decision number.
+     *
+     * @return int
+     */
+    public function getDecisionNumber()
+    {
+        return $this->number;
+    }
+
+    /**
+     * Get the number.
+     *
+     * @return int
+     */
+    public function getNumber()
+    {
+        return $this->number;
+    }
+
+    /**
+     * Set the number.
+     *
+     * @param int $number
+     */
+    public function setNumber($number)
+    {
+        $this->number = $number;
+    }
+
+    /**
+     * Get the reference.
+     *
+     * @return SubDecision
+     */
+    public function getReference()
+    {
+        return $this->reference;
+    }
+
+    /**
+     * Set the reference.
+     *
+     * @param Reference $reference
+     */
+    public function setReference(Reference $reference)
+    {
+        $this->reference = $reference;
+    }
+
+    /**
+     * Get the abbreviation.
+     *
+     * @return string
+     */
+    public function getAbbr()
+    {
+        return $this->abbr;
+    }
+
+    /**
+     * Set the abbreviation.
+     *
+     * @param string $abbr
+     */
+    public function setAbbr($abbr)
+    {
+        $this->abbr = $abbr;
+    }
+
+    /**
+     * Get the name.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the name.
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get the function.
+     *
+     * @return string
+     */
+    public function getFunction()
+    {
+        return $this->function;
+    }
+
+    /**
+     * Set the function.
+     *
+     * @todo Make sure that the function is of an allowed value
+     *
+     * @param string $function
+     */
+    public function setFunction($function)
+    {
+        $this->function = $function;
+    }
+
+    /**
+     * Get the member.
+     *
+     * @return Member
+     */
+    public function getMember()
+    {
+        return $this->member;
+    }
+
+    /**
+     * Set the member.
+     *
+     * @param Member $member
+     */
+    public function setMember(Member $member)
+    {
+        $this->member = $member;
+    }
+
+    /**
+     * Get the content.
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set the content.
+     *
+     * @param string $content
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * Get the type.
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set the type.
+     *
+     * @todo Make sure that the type is of an allowed value
+     *
+     * @param string $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
 }
