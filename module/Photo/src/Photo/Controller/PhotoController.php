5d8bd7acae1f4e76d31034a1a12b94a22c1c8111
s136986 <s136986@S136986.campus.tue.nl>
2014-07-06 21:15:45 +0200

    implemented basic album listing controller

--- module/Photo/src/Photo/Controller/PhotoController.php
+++ module/Photo/src/Photo/Controller/PhotoController.php
@@ -6,12 +6,24 @@ use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
 class PhotoController extends AbstractActionController {
-	
-	public function indexAction() {
-	
-	$vm = new ViewModel();
-	return $vm;
-	
-	}
+
+    public function indexAction() {
+        $service = $this->getAlbumService();
+        $albums = $service->getAlbums();
+            return new ViewModel(array(
+                'albums' => $albums
+            ));
+            
+        
+    }
+
+    /**
+     * Gets the album service.
+     * 
+     * @return Photo\Service\Album
+     */
+    public function getAlbumService() {
+        return $this->getServiceLocator()->get("photo_service_album");
+    }
 
 }
