9cfc720baa1ab9f841fb0d12603b288404f1d235
Geert Derks <g.a.f.derks@student.tue.nl>
2014-08-15 12:24:17 +0200

    Created a Model file with the Job class.
    Methods still have to be created.

---
+++ module/Company/src/Company/Model/Job.php
@@ -0,0 +1,208 @@
+<?php
+
+namespace Company\Model;
+
+use Doctrine\ORM\Mapping as ORM;
+//use Doctrine\Common\Collections\ArrayCollection;
+//use Zend\Permissions\Acl\Role\RoleInterface;
+//use Zend\Permissions\Acl\Resource\ResourceInterface;
+
+/**
+ * Job model.
+ *
+ * @ORM\Entity
+ */
+class Job //implements RoleInterface, ResourceInterface
+{
+
+    /**
+     * The job id.
+     *
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    /**
+     * The job's name.
+     *
+     * @ORM\Column(type="string")
+     */
+    protected $name;
+    
+    /**
+     * The job's status.
+     *
+     * @ORM\Column(type="boolean")
+     */
+    protected $active;
+
+    /**
+     * The job's website.
+     *
+     * @ORM\Column(type="string")
+     */
+    protected $website;
+
+    /**
+     * The job's phone.
+     *
+     * @ORM\Column(type="string")
+     */
+    protected $phone;
+
+    /**
+     * The job's email.
+     *
+     * @ORM\Column(type="string")
+     */
+    protected $email;
+    
+    /**
+     * The job's description.
+     *
+     * @ORM\Column(type="string")
+     */
+    protected $description;
+    
+        
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        // todo
+    }
+
+    /**
+     * Get the job's id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the job's name.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    
+    /**
+     * Set the job's name.
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get the job's status.
+     *
+     * @return boolean
+     */
+    public function getActive()
+    {
+        return $this->active;
+    }
+    
+    /**
+     * Set the company's address.
+     *
+     * @param string $address
+     */
+    public function setActive($active)
+    {
+        $this->active = $active;
+    }
+    
+    /**
+     * Get the job's website.
+     *
+     * @return string
+     */
+    public function getWebsite()
+    {
+        return $this->website;
+    }
+    
+    /**
+     * Set the job's website.
+     *
+     * @param string $website
+     */
+    public function setWebsite($website)
+    {
+        $this->website = $website;
+    }
+        
+    /**
+     * Get the job's phone.
+     *
+     * @return string
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+    
+    /**
+     * Set the job's phone.
+     *
+     * @param string $phone
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+    }
+    
+    /**
+     * Get the job's email.
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+    
+    /**
+     * Set the job's email.
+     *
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+    
+    /**
+     * Get the job's description.
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+    
+    /**
+     * Set the job's description.
+     *
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+    
+    
+}
