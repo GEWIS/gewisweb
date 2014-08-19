e63c188a51010af1da9ce40eacfc7c71c919df74
Geert Derks <g.a.f.derks@student.tue.nl>
2014-08-19 22:51:59 +0200

    Created Model files for CompanyPackets

---
+++ module/Company/src/Company/Model/CompanyFeaturedPacket.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Company\Model;
+
+use Doctrine\ORM\Mapping as ORM;
+//use Doctrine\Common\Collections\ArrayCollection;
+//use Zend\Permissions\Acl\Role\RoleInterface;
+//use Zend\Permissions\Acl\Resource\ResourceInterface;
+
+/**
+ * CompanyFeaturedPacket model.
+ *
+ * @ORM\Entity
+ */
+class CompanyFeaturedPacket extends CompanyPacket //implements RoleInterface, ResourceInterface
+{
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        // todo
+    }
+
+    public function publish()
+    {
+        // todo
+    }
+    
+    public function unpublish()
+    {
+        // todo
+    }
+    
+    public function create()
+    {
+        // todo
+    }
+    
+    public function save()
+    {
+        // todo   
+    }
+    
+    public function delete()
+    {
+        // todo
+    }
+    
+    
+}
