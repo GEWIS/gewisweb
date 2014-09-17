d4cf6ce982d870bd0db2e28173146078561781cf
Geert Derks <g.a.f.derks@student.tue.nl>
2014-09-17 11:38:03 +0200

    Changed comment

--- module/Company/src/Company/Service/Company.php
+++ module/Company/src/Company/Service/Company.php
@@ -7,13 +7,14 @@ use Application\Service\AbstractService;
 use Company\Model\Company as CompanyModel;
 use Company\Mapper\Company as CompanyMapper;
 /**
- * User service.
+ * Company service.
  */
 class Company extends AbstractService
 {
     public function getCompanyList() {
         return $this->getCompanyMapper()->findAll();
     }
+    
     public function getCompanyMapper()
     {
         return $this->sm->get('company_mapper_company');
