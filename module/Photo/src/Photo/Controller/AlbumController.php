02f772f188ec909165cad199b0c85702c7c5f54f
Justin Szanto <j.b.szanto@student.tue.nl>
2014-12-10 22:38:33 +0100

    Restructure controllers

--- module/Photo/src/Photo/Controller/AlbumController.php
+++ module/Photo/src/Photo/Controller/AlbumController.php
@@ -1,3 +1,42 @@
 <?php
 
+namespace Photo\Controller;
 
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class AlbumController extends AbstractActionController
+{
+
+    public function indexAction()
+    {
+        $album_service = $this->getAlbumService();
+        $albums = $album_service->getAlbums();
+        $photo_service = $this->getPhotoService();
+
+        return new ViewModel(array(
+            'albums' => $albums
+        ));
+    }
+
+    /**
+     * Gets the album service.
+     * 
+     * @return Photo\Service\Album
+     */
+    public function getAlbumService()
+    {
+        return $this->getServiceLocator()->get("photo_service_album");
+    }
+
+    /**
+     * Gets the photo service.
+     * 
+     * @return Photo\Service\Photo
+     */
+    public function getPhotoService()
+    {
+        return $this->getServiceLocator()->get("photo_service_photo");
+    }
+
+}
