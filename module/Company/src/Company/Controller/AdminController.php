034b422df93942d5457e27099acbcf21cfe3ec77
Wessel Sinnema <w.t.h.sinnema@student.tue.nl>
2015-02-01 18:17:22 +0100

    Fixed the admin company edit page, but the changes are not persistent yet

--- module/Company/src/Company/Controller/AdminController.php
+++ module/Company/src/Company/Controller/AdminController.php
@@ -20,15 +20,47 @@ class AdminController extends AbstractActionController
 
     }
 
+    public function saveAction()
+    {
+        $companyName = $this->params('asciiCompanyName');    
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $companyService = $this->getCompanyService();
+            $companyForm=$companyService->getCompanyForm();
+            $companyForm->setData($request->getPost());
+
+            // TODO: filter not yet working:
+            // \I18n\Filter component requires the intl PHP extension
+            //
+            //if ($companyForm->isValid()) {
+            //    echo 'success';
+                $companyService->saveCompany();
+            //}
+        }
+
+        return $this->redirect()->toRoute('admin_company/default', array('action'=>'edit', 'asciiCompanyName'=>$companyName),array(),true);   
+    }
     public function editAction()
     {
         $companyService = $this->getCompanyService();
         
         $companyName = $this->params('asciiCompanyName');    
         $jobName = $this->params('asciiJobName');    
+        $companyForm=$companyService->getCompanyForm();
+        $company_list = $companyService->getEditableCompaniesWithAsciiName($companyName);
+        //echo($this->url()->fromRoute('admin_company/default',array('action'=>'save', 'asciiCompanyName'=>$companyName)));
+        if (empty($company_list)){
+            $company=null;
+        }
+        else {
+            $company=$company_list[0];
+            $companyForm->bind($company);
+            $companyForm->setAttribute('action',$this->url()->fromRoute('admin_company/default',array('action'=>'save','asciiCompanyName'=>$companyName)));
+        }
         $vm = new ViewModel(array(
-            'company_list' => $companyService->getEditableCompaniesWithAsciiName($companyName),
-            'asciiJobName' => $jobName
+            'company' => $company,
+            'asciiJobName' => $jobName,
+            'companyEditForm' => $companyForm,
         ));
         
         return $vm;
