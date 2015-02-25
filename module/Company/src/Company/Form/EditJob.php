2ced2722314ba10fbe1998f577e0cd351ced8fd3
Geert Derks <g.a.f.derks@student.tue.nl>
2015-02-25 11:33:42 +0100

    Worked on Form for JobEditor.

--- module/Company/src/Company/Form/EditJob.php
+++ module/Company/src/Company/Form/EditJob.php
@@ -1,9 +1,9 @@
 <?php
 namespace Company\Form;
 
+use Zend\Form\Element;
 use Zend\Form\Form;
 use Zend\InputFilter\InputFilter;
-//use Zend\I18n\Translator\TranslatorInterface as Translator;
 use Zend\Mvc\I18n\Translator as Translator;
 
 class EditJob extends Form
@@ -15,7 +15,13 @@ class EditJob extends Form
         
         $this->setAttribute('method', 'post');
         $this->add(array(
-            'name' => 'id',
+            'name' => 'job-id',
+            'attributes' => array(
+                'type'  => 'hidden',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'company-id',
             'attributes' => array(
                 'type'  => 'hidden',
             ),
@@ -30,6 +36,7 @@ class EditJob extends Form
             ),
         ));
         $this->add(array(
+<<<<<<< HEAD
             'name' => 'asciiName',
             'attributes' => array(
                 'type'  => 'text',
@@ -43,9 +50,14 @@ class EditJob extends Form
             'attributes' => array(
  //               'type'  => 'boolean',
                 'type' => 'Checkbox',
+=======
+            'name' => 'address',
+            'attributes' => array(
+                'type'  => 'text',
+>>>>>>> Worked on Form for JobEditor.
             ),
             'options' => array(
-                'label' => $translate->translate('Active'),
+                'label' => $translate->translate('Location'),
             ),
         ));
         $this->add(array(
@@ -59,6 +71,7 @@ class EditJob extends Form
         ));
         $this->add(array(
             'name' => 'email',
+            'type' => 'Zend\Form\Element\Email',
             'attributes' => array(
                 'type'  => 'text',
             ),
@@ -77,11 +90,22 @@ class EditJob extends Form
         ));
         $this->add(array(
             'name' => 'description',
+<<<<<<< HEAD
             'attributes' => array(
                 'type'  => 'textarea',
             ),
+=======
+            'type' => 'Zend\Form\Element\Textarea',
+>>>>>>> Worked on Form for JobEditor.
             'options' => array(
                 'label' => $translate->translate('Description'),
+            ),
+        ));
+        $this->add(array(
+            'name' => 'active',
+            'type' => 'Zend\Form\Element\Checkbox',
+            'options' => array(
+                'label' => $translate->translate('Active')
             ),
         ));
         $this->add(array(
@@ -109,20 +133,46 @@ class EditJob extends Form
                     'name' => 'string_length',
                     'options' => array(
                         'min' => 2,
-                        'max' => 255
+                        'max' => 127
                     )
-                ),
-                array('name' => 'alnum')
+                )
             )
         ));
-
-//        $filter->add(array(
-//            'name' => 'date',
-//            'required' => true,
-//            'validators' => array(
-//                array('name' => 'date')
-//            )
-//        ));
+        
+        $filter->add(array(
+            'name' => 'website',
+            'required' => true,
+            'validators' => array(
+                array(
+                    'name' => 'regex',
+                    'options' => array(
+                        'pattern' => '_^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)(?:\.(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)*(?:\.(?:[a-z\x{00a1}-\x{ffff}]{2,})))(?::\d{2,5})?(?:/[^\s]*)?$_iu'
+                     )     
+                )
+            )
+        ));
+        
+        $filter->add(array(
+            'name' => 'description',
+            'required' => true,
+            'validators' => array(
+                array(
+                    'name' => 'string_length',
+                    'options' => array(
+                        'min' => 2,
+                        'max' => 10000
+                    )
+                )
+            )
+        ));
+        
+        $filter->add(array(
+            'name' => 'email',
+            'required' => true,
+            'validators' => array(
+                array('name' => 'email_address'),
+            )
+        ));
 
         $filter->add(array(
             'name' => 'logo',
