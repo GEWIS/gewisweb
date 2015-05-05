b5bdbebc849c489c12abb2fe30c3423f766068a7
Justin Szanto <j.b.szanto@student.tue.nl>
2015-05-05 23:11:27 +0200

    Document doctrine extensions

--- extensions/doctrine/Numeric/Rand.php
+++ extensions/doctrine/Numeric/Rand.php
@@ -3,7 +3,11 @@ namespace Numeric;
 use Doctrine\ORM\Query\AST\Functions\FunctionNode;
 use Doctrine\ORM\Query\Lexer;
 /**
- * RandFunction ::= "RAND" "(" ")"
+ * This extension adds SQL RAND() functionality to DQL for returning random
+ * database entries.
+ * This code originates from https://gist.github.com/Ocramius/919465
+ *
+ * Usage: Call addSelect('RAND() as HIDDEN rand') and orderBy('rand') on a QueryBuilder object
  */
 class Rand extends FunctionNode
 {
