8a49d4d2cae59f7a11d1f5c9cd0a1b9098ff03f9
Pieter Kokx <pieter@kokx.nl>
2015-07-29 19:53:49 +0200

    Added action for searching decisions.

--- module/Decision/src/Decision/Controller/DecisionController.php
+++ module/Decision/src/Decision/Controller/DecisionController.php
@@ -19,6 +19,14 @@ class DecisionController extends AbstractActionController
     }
 
     /**
+     * Search decisions.
+     */
+    public function searchAction()
+    {
+        return new ViewModel(array());
+    }
+
+    /**
      * Get the organ service.
      */
     public function getOrganService()
