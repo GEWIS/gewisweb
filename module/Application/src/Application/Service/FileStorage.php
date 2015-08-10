de82b5c630ec5428e907ba5de228c420e0c5407e
Justin Szanto <j.b.szanto@student.tue.nl>
2015-08-10 16:08:06 +0200

    Add seperate function for storing uploaded files

--- module/Application/src/Application/Service/FileStorage.php
+++ module/Application/src/Application/Service/FileStorage.php
@@ -2,7 +2,12 @@
 
 namespace Application\Service;
 
-
+/**
+ * File storage service. This service can be used to safely store files without
+ * having to worry about file names.
+ *
+ * @package Application\Service
+ */
 class FileStorage extends AbstractService
 {
     /**
@@ -31,27 +36,54 @@ class FileStorage extends AbstractService
     }
 
     /**
+     * Stores an uploaded file in the content based file system.
+     *
+     * @param array $file
+     *
+     * @return string The CFS path at which the file was stored
+     * @throws \Exception
+     */
+    public function storeUploadedFile($file)
+    {
+        $config = $this->getConfig();
+        if ($file['error'] !== 0) {
+            throw new \Exception(
+                sprintf(
+                    $this->getTranslator()->translate('An unknown error occurred during uploading (%i)'),
+                    $file['error']
+                )
+            );
+        }
+
+        $extension = pathinfo($file['name'], PATHINFO_EXTENSION);
+        $storagePath = $this->generateStoragePath($file['tmp_name']);
+        $destination = $config['storage_dir'] . '/' .  $storagePath . '.' . $extension;
+        if (!file_exists($destination)) {
+            move_uploaded_file($file['tmp_name'], $destination);
+        }
+
+        return $storagePath;
+
+    }
+
+    /**
      * Stores files in the content based file system
      *
-     * @param string $source
-     * @param bool $isUploaded Indicates whether the file to be stored is an uploaded file.
-     * @param string $extension optional extension in the case that the file has no extension
+     * @param string $source The source file to store
+     * @param boolean $move indicating wether the file should be moved or copied
      *
      * @return string the path at which the file was stored.
      */
-    public function storeFile($source, $isUploaded = true, $extension = null)
+    public function storeFile($source, $move = true)
     {
         $config = $this->getConfig();
         $storagePath = $this->generateStoragePath($source);
-        if (is_null($extension)) {
-            $parts = explode('.', $source);
-            $extension = end($parts);
-        }
+        $extension = pathinfo($source, PATHINFO_EXTENSION);
 
         $destination = $config['storage_dir'] . '/' .  $storagePath . '.' . $extension;
         if(!file_exists($destination)) {
-            if($isUploaded) {
-                move_uploaded_file($source, $destination);
+            if($move) {
+                rename($source, $destination);
             } else {
                 copy($source, $destination);
             }
@@ -60,6 +92,13 @@ class FileStorage extends AbstractService
         return $storagePath;
     }
 
+    /**
+     * Removes a file from the content based file system.
+     *
+     * @param string $path The CFS path of the file to remove
+     *
+     * @return bool indicating if removing the file was successful.
+     */
     public function removeFile($path)
     {
         $config = $this->getConfig();
