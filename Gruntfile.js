ec6c71f98c8b00b59338bd9e3147ea04f52197ed
Pieter Kokx <pieter@kokx.nl>
2015-11-21 18:54:11 +0100

    Added watch command for css changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,10 +11,20 @@ module.exports = function(grunt) {
                     'public/css/gewis-theme.css': 'public/css/gewis-theme.scss'
                 }
             }
+        },
+        watch: {
+            css: {
+                files: [ 'public/css/gewis-theme.scss' ],
+                tasks: 'css'
+            }
+
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('default', ['sass:gewis']);
+    grunt.registerTask('css', ['sass:gewis']);
+
+    grunt.registerTask('default', ['css']);
 };
