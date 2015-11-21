581614aa357bb03d2f58fd8eee012c0c3324a5c9
Justin Szanto <j.b.szanto@student.tue.nl>
2015-11-21 23:08:26 +0100

    Add poll creation form

---
+++ module/Frontpage/src/Frontpage/Form/Poll.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Frontpage\Form;
+
+use Zend\Form\Form;
+use Zend\InputFilter\InputFilter;
+use Zend\InputFilter\InputFilterProviderInterface;
+use Zend\I18n\Translator\TranslatorInterface as Translator;
+
+class Poll extends Form implements InputFilterProviderInterface
+{
+
+    public function __construct(Translator $translator)
+    {
+        parent::__construct();
+
+        $this->add(array(
+            'name' => 'dutchQuestion',
+            'type' => 'text',
+            'options' => array(
+                'label' => $translator->translate('Dutch question')
+            )
+        ));
+
+        $this->add(array(
+            'name' => 'englishQuestion',
+            'type' => 'text',
+            'options' => array(
+                'label' => $translator->translate('English question')
+            )
+        ));
+
+        $this->add(array(
+            'name' => 'submit',
+            'type' => 'submit',
+            'attributes' => array(
+                'value' => $translator->translate('Submit')
+            )
+        ));
+    }
+
+    /**
+     * Should return an array specification compatible with
+     * {@link Zend\InputFilter\Factory::createInputFilter()}.
+     *
+     * @return array
+     */
+    public function getInputFilterSpecification()
+    {
+        return array(
+            'dutchQuestion' => array(
+                'required' => true,
+                'validators' => array(
+                    array(
+                        'name' => 'string_length',
+                        'options' => array(
+                            'min' => 3,
+                            'max' => 75
+                        )
+                    ),
+                ),
+            ),
+
+            'englishQuestion' => array(
+                'required' => true,
+                'validators' => array(
+                    array(
+                        'name' => 'string_length',
+                        'options' => array(
+                            'min' => 3,
+                            'max' => 75
+                        )
+                    ),
+                ),
+            ),
+        );
+    }
+}
