5dbcd6b356b36c1764e68001842f67bcaeb7eb4b
Justin Szanto <jszanto@users.noreply.github.com>
2015-11-26 21:02:01 +0100

    Display polls on the frontpage

--- module/Frontpage/src/Frontpage/Controller/FrontpageController.php
+++ module/Frontpage/src/Frontpage/Controller/FrontpageController.php
@@ -3,13 +3,17 @@
 namespace Frontpage\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
+use Zend\Session\Container as SessionContainer;
 use Zend\View\Model\ViewModel;
 
 class FrontpageController extends AbstractActionController
 {
     public function homeAction()
     {
-        return new ViewModel($this->getFrontpageService()->getHomepageData());
+        $homePageData = $this->getFrontpageService()->getHomepageData();
+        $session = new SessionContainer('lang');
+        $homePageData['lang'] = $session->lang;
+        return new ViewModel($homePageData);
     }
 
     /**
