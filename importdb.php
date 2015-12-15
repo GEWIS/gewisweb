79bd0805dd09c164e162e093f6cf915d759bde01
Pieter Kokx <pieter@kokx.nl>
2015-12-16 00:17:28 +0100

    Dramatically improved import script

--- importdb.php
+++ importdb.php
@@ -8,15 +8,17 @@
 
 // connections
 $pgconn = new PDO('pgsql:host=localhost;dbname=gewis_report;user=;password=');
+$pgconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $myconn = new PDO('mysql:host=localhost;dbname=gewisweb_dev', '', '');
+$myconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 /* which tables to sync */
 $tables = [
     'Address',
-    'Boardmember',
+    'BoardMember',
     'Decision',
-    'Mailinglist',
+    'MailingList',
     'Meeting',
     'Member',
     'members_mailinglists',
@@ -26,9 +28,16 @@ $tables = [
     'SubDecision'
 ];
 
+// to not trip up InnoDB
+$myconn->query('SET foreign_key_checks = 0');
+
 foreach ($tables as $table) {
     $query = "SELECT * FROM $table";
     $stmt = $pgconn->query($query);
+    echo "Table $table\n";
+
+    $truncate = "TRUNCATE TABLE $table";
+    $myconn->query($truncate);
 
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
         $fields = '(' . implode(', ', array_keys($row)) . ')';
@@ -36,12 +45,26 @@ foreach ($tables as $table) {
             return ':' . $a;
         }, array_keys($row))) . ')';
 
-        $truncate = "TRUNCATE TABLE $table";
-        $myconn->query($truncate);
+        $data = $row;
+
+        // see if we can fetch about 256 more rows (gigantic speed increase)
+        for ($i = 0; $i < 256 && ($row2 = $stmt->fetch(PDO::FETCH_ASSOC)); $i++) {
+            $values .= ', (' . implode(', ', array_map(function ($a) use ($i) {
+                return ':' . $a . $i;
+            }, array_keys($row2))) . ')';
+            foreach ($row2 as $key => $value) {
+                $data[$key . $i] = $value;
+            }
+        }
 
         $sql = "INSERT INTO $table $fields VALUES $values";
         $stmtt = $myconn->prepare($sql);
 
-        $stmtt->execute($row);
+        $stmtt->execute($data);
+
+        echo '.';
     }
+    echo "\n\n";
 }
+
+$myconn->query('SET foreign_key_checks = 1');
