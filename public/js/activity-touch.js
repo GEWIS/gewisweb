845f0f87532a56b48580faad972a43e44696546c
Justin Szanto <jszanto@users.noreply.github.com>
2015-12-17 13:53:02 +0100

    Implement activity listing on activity screen and cleanup

--- public/js/activity-touch.js
+++ public/js/activity-touch.js
@@ -1,6 +1,7 @@
 Activity= {};
 
 Activity.Touch = {};
+Activity.Touch.activities = [];
 
 Activity.Touch.init = function () {
     $('.btn-keypad').each(function (index) {
@@ -32,7 +33,11 @@ Activity.Touch.init = function () {
     $('#loginModal').on('hidden.bs.modal', function () {
         $('#lidnrInput').val('');
         $('#pinInput').val('');
-    })
+    });
+    Activity.Touch.fetchActivities();
+    $('#activityList').on( 'tap', 'tr', function() {
+        Activity.Touch.showActivity($( this ).data('activity-index'));
+    });
 };
 
 Activity.Touch.Login = function(lidnr, pincode) {
@@ -40,9 +45,38 @@ Activity.Touch.Login = function(lidnr, pincode) {
     $("#loginFailed").hide();
     $.post(URLHelper.url('user/pinlogin'), {'lidnr': lidnr, 'pincode': pincode}, function (data) {
         if(data.login) {
-            $('#loginModal').modal('hide')
+            $('#loginModal').modal('hide');
         } else {
             $("#loginFailed").show();
         }
     });
+};
+
+
+Activity.Touch.fetchActivities = function () {
+    $.getJSON(URLHelper.url('activity_api/list'), function (data) {
+       Activity.Touch.activities = data;
+        $.each(data, function(index, activity) {
+            $('#activityList').append(
+                '<tr data-activity-index="' + index + '">'
+                + '<td>' + activity.beginTime.date.replace(':00.000000', '') + '</td>'
+                + '<td>' + activity.endTime.date.replace(':00.000000', '') + '</td>'
+                + '<td>' + activity.name + '</td>'
+                + '<td>' + activity.costs + '</td>'
+                + '</tr>'
+            );
+        });
+        });
+};
+
+Activity.Touch.showActivity = function (index) {
+    var activity = Activity.Touch.activities[index];
+    $('#activityModal').modal('show');
+    $('#activityModalLabel').html(activity.name);
+    $('#activityDescription').html(activity.description);
+    $('#activityDateTime').html(activity.beginTime.date.replace(':00.000000', ''));
+    $('#activityLocation').html(activity.location);
+    $('#activityCosts').html(activity.costs);
+    $('#activityAttendeeCount').html(activity.attendees.length);
+
 };
