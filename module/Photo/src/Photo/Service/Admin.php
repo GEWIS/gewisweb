fae4f6afdbf5dd04dfcf905d1aacdf32052986fa
Geert Derks <g.a.f.derks@student.tue.nl>
2015-12-23 14:15:52 +0100

    Styled the administrator interface of the photo module a bit

--- module/Photo/src/Photo/Service/Admin.php
+++ module/Photo/src/Photo/Service/Admin.php
@@ -178,7 +178,7 @@ class Admin extends AbstractAclService
             }
         } else {
             throw new \Exception(
-                $translator->translate('The uploaded file is not a valid image')
+                $translator->translate("The uploaded file is not a valid image \nError: " . json_encode($imageValidator->getMessages()))
             );
         }
     }
