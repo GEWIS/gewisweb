338fad8bf2f054c93220d699b3fa4a6910a39074
Armand Duijn <armand@duijn.space>
2015-12-23 12:46:00 +0100

    Another uninteresting commit

---
+++ public/js/etc.js
@@ -0,0 +1,21 @@
+(function ($) {
+    $(function() {
+        var today = new Date();
+
+        if (today.getMonth() === 3 && today.getDate() === 1) { // 1 April
+            $("a[href^='mailto:klachten@gewis.nl']")
+                .mouseenter(function(){
+                    $("<audio></audio>").attr({
+                        "id": "na-na-na",
+                        "src": "/etc/na-na-na.mp3",
+                        "volume": 1,
+                        "autoplay": "autoplay",
+                        "loop": "loop"
+                    }).appendTo("body");
+                })
+                .mouseleave(function(){
+                    $("#na-na-na").remove();
+                });
+        }
+    });
+}(jQuery));
