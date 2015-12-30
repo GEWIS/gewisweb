b7b02af17dc7e93232c85420eac66ffc7b14ab11
Justin Szanto <jszanto@users.noreply.github.com>
2015-12-30 23:54:28 +0100

    Add basic production config

--- config/application.config.php
+++ config/application.config.php
@@ -1,22 +1,28 @@
 <?php
-return [
-    // This should be an array of module namespaces used in the application.
-    'modules' => [
-        'ZendDeveloperTools',
-        'DoctrineModule',
-        'DoctrineORMModule',
-        'User',
-        'Application',
-        'Company',
-        'Decision',
-        'Frontpage',
-        'Activity',
-        'Photo',
-        'Education',
-        'Migration',
-        'MaglMarkdown'
-    ],
+$env = getenv('APP_ENV') ?: 'production';
 
+// This should be an array of module namespaces used in the application.
+$modules = [
+    'DoctrineModule',
+    'DoctrineORMModule',
+    'User',
+    'Application',
+    'Company',
+    'Decision',
+    'Frontpage',
+    'Activity',
+    'Photo',
+    'Education',
+    'Migration',
+    'MaglMarkdown'
+];
+
+if ($env == 'development') {
+    $modules[] = 'ZendDeveloperTools';
+}
+
+return [
+    'modules' => $modules,
     // These are various options for the listeners attached to the ModuleManager
     'module_listener_options' => [
         // This should be an array of paths in which modules reside.
@@ -27,21 +33,19 @@ return [
             './module',
             './vendor',
         ],
-
         // An array of paths from which to glob configuration files after
         // modules are loaded. These effectively override configuration
         // provided by modules themselves. Paths may use GLOB_BRACE notation.
         'config_glob_paths' => [
             'config/autoload/{,*.}{global,local}.php',
         ],
-
         // Whether or not to enable a configuration cache.
         // If enabled, the merged configuration will be cached and used in
         // subsequent requests.
-        //'config_cache_enabled' => $booleanValue,
+        'config_cache_enabled' => ($env === 'production'),
 
         // The key used to create the configuration cache file name.
-        //'config_cache_key' => $stringKey,
+        'config_cache_key' => 'config.cache',
 
         // Whether or not to enable a module class map cache.
         // If enabled, creates a module class map cache which will be used
@@ -52,14 +56,13 @@ return [
         //'module_map_cache_key' => $stringKey,
 
         // The path in which to cache merged configuration.
-        //'cache_dir' => $stringPath,
+        'cache_dir' => 'data/cache',
 
         // Whether or not to enable modules dependency checking.
         // Enabled by default, prevents usage of modules that depend on other modules
         // that weren't loaded.
-        // 'check_dependencies' => true,
+        'check_dependencies' => ($env === 'development'),
     ],
-
     // Used to create an own service manager. May contain one or more child arrays.
     //'service_listener_options' => array(
     //     array(
@@ -70,7 +73,7 @@ return [
     //     ),
     // )
 
-   // Initial configuration with which to seed the ServiceManager.
-   // Should be compatible with Zend\ServiceManager\Config.
-   // 'service_manager' => array(),
+    // Initial configuration with which to seed the ServiceManager.
+    // Should be compatible with Zend\ServiceManager\Config.
+    // 'service_manager' => array(),
 ];
