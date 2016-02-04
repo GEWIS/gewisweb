9bff5a77327a2da1d2b3bfd2a7a68f0e8689b0df
Pieter Kokx <pieter@kokx.nl>
2016-02-04 22:28:41 +0100

    Added password change form

---
+++ module/User/src/User/Form/Password.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace User\Form;
+
+use Zend\Form\Form;
+use Zend\I18n\Translator\TranslatorInterface as Translator;
+use Zend\InputFilter\InputFilterProviderInterface
+use Zend\Authentication\Result;
+
+class Password extends Form implements InputFilterProviderInterface
+{
+
+    public function __construct(Translator $translate)
+    {
+        parent::__construct();
+
+        $this->add([
+            'name' => 'old_password',
+            'type' => 'old_password',
+            'options' => [
+                'label' => $translate->translate('Old password')
+            ]
+        ]);
+
+        $this->add([
+            'name' => 'password',
+            'type' => 'password',
+            'options' => [
+                'label' => $translate->translate('New password')
+            ]
+        ]);
+
+        $this->add([
+            'name' => 'password_verify',
+            'type' => 'password',
+            'options' => [
+                'label' => $translate->translate('Verify new password')
+            ]
+        ]);
+
+        $this->add([
+            'name' => 'submit',
+            'type' => 'submit',
+            'attributes' => [
+                'value' => $translate->translate('Change password')
+            ]
+        ]);
+        $this->get('submit')->setLabel($translate->translate('Change password'));
+    }
+
+    public function getInputFilterSpecification()
+    {
+        return [
+            'password' => [
+                'required' => true,
+                'validators' => [
+                    ['name' => 'not_empty'],
+                    [
+                        'name' => 'string_length',
+                        'options' => [
+                            'min' => 8
+                        ]
+                    ]
+                ]
+            ],
+            'password_verify' => [
+                'required' => true,
+                'validators' => [
+                    ['name' => 'not_empty'],
+                    [
+                        'name' => 'string_length',
+                        'options' => [
+                            'min' => 8
+                        ]
+                    ],
+                    [
+                        'name' => 'identical',
+                        'options' => [
+                            'token' => 'password'
+                        ]
+                    ]
+                ]
+            ]
+        ];
+    }
+}
