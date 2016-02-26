8e8a5954b6328dcd4f701ec1856a6e67272248f0
Justin Szanto <jszanto@users.noreply.github.com>
2016-02-26 22:39:48 +0100

    Improve company listing in company admin

---
+++ public/js/company-admin.js
@@ -0,0 +1,56 @@
+Company = {};
+
+Company.Admin = {
+    init: function () {
+        $("#filterCompanies").keyup(function() {
+            filterCompanies($(this).val());
+        });
+    },
+
+    filterCompanies: function () {
+        var term = $('#filterCompanies').val();
+        $(".company-list-item").each(function() {
+            if ($(this).text().toLowerCase().indexOf(term.toLowerCase())==-1) {
+                $(this).css({display: 'none'});
+            } else {
+                $(this).css({display: 'table-row'});
+            }
+        });
+    },
+
+    sortCompaniesByName: function(order) {
+        var companies = $(".company-list-item");
+        companies.sort(function(a,b) {
+
+            if ($(a).find('.company-name > a').html() > $(b).find('.company-name > a').html()) {
+                return order;
+            } else {
+                return -1 * order;
+            }
+
+        });
+        companies.detach().appendTo($(".company-list"));
+    },
+
+    sortCompaniesByColumn: function(order, column) {
+        var companies = $(".company-list-item");
+        companies.sort(function(a,b) {
+            console.log($($(a).find('td')[column]).html());
+            if ($($(a).find('td')[column]).html() > $($(b).find('td')[column]).html()) {
+                return order;
+            } else {
+                return -1 * order;
+            }
+
+        });
+        companies.detach().appendTo($(".company-list"));
+    },
+
+    deleteCompany: function (id) {
+        $("#deleteForm").attr('action', URLHelper.url('admin_company/delete', {'company_id': id}));
+    },
+
+    hideCompany: function (id) {
+
+    }
+};
