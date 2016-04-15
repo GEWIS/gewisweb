16cefcf686d4feccda234b993ea474b1d01fe69d
Stefan Majoor <stefan@codeyellow.nl>
2016-04-15 14:56:59 +0200

    send an email on activity creation

--- module/Activity/src/Activity/Service/Activity.php
+++ module/Activity/src/Activity/Service/Activity.php
@@ -116,6 +116,8 @@ class Activity extends AbstractAclService implements ServiceManagerAwareInterfac
         $em->persist($activity);
         $em->flush();
 
+        $this->getEmailService()->sendActivityCreationEmail($activity);
+
         return $activity;
     }
 
@@ -313,5 +315,15 @@ class Activity extends AbstractAclService implements ServiceManagerAwareInterfac
     public function getActivityMapper()
     {
         return $this->sm->get('activity_mapper_activity');
+    }
+
+        /**
+     * Get the email service.
+     *
+     * @return EmailService
+     */
+    public function getEmailService()
+    {
+        return $this->sm->get('activity_service_email');
     }
 }
