3bf82048f69ee1a935f79b60062b734ce411c537
Koen Klaren <koen.klaren@gmail.com>
2016-09-29 12:01:43 +0200

    Update styleguide

--- styleguide/template/helpers/helpers.js
+++
@@ -1,8 +0,0 @@
-module.exports.register = function (handlebars, config) {
-    config = config || {};
-
-    handlebars.registerHelper('ifEqual', function (a, b, options) {
-        console.log(a == b, a, b);
-        return (a == b) ? options.fn(this) : options.inverse(this);
-    });
-};
