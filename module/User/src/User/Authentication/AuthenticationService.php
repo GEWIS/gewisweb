00a14c00cb2cbcc665c93d60a73c266458d47300
Justin Szanto <jszanto@users.noreply.github.com>
2016-10-02 20:42:58 +0200

    Save some queries by caching a result

--- module/User/src/User/Authentication/AuthenticationService.php
+++ module/User/src/User/Authentication/AuthenticationService.php
@@ -6,13 +6,18 @@ use \Zend\Authentication\AuthenticationService as ZendAuthService;
 
 class AuthenticationService extends ZendAuthService
 {
+    protected $identity = null;
     public function getIdentity()
     {
+        if ($this->identity !== null) {
+            return $this->identity;
+        }
         $storage = $this->getStorage();
         if ($storage->isEmpty()) {
             return;
         }
         $mapper = $this->getAdapter()->getMapper();
-        return $mapper->findByLidnr($storage->read()->getLidnr());
+        $this->identity = $mapper->findByLidnr($storage->read()->getLidnr());
+        return $this->identity;
     }
 }
