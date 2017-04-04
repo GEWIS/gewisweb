bbc6efcb831c9189a93b7b2647bba76d50e5ca98
Staartvin <mijnleraar@msn.com>
2017-04-04 14:20:07 +0200

    Send email when checkbox is checked.

--- module/Activity/src/Activity/Service/Activity.php
+++ module/Activity/src/Activity/Service/Activity.php
@@ -283,11 +283,19 @@ class Activity extends AbstractAclService implements ServiceManagerAwareInterfac
         $em->persist($activity);
         $em->flush();
 
-        //$this->getEmailService()->sendActivityCreationEmail($activity);
+        // Send an email when a new Activity was created.
         $this->getEmailService()->sendEmail('activity_creation', 'email/activity',
             'Nieuwe activiteit aangemaakt op de GEWIS website | New activity was created on the GEWIS website',
             ['activity' => $activity]);
 
+        // Send email to GEFLITST if user checked checkbox of GEFLITST
+        if ($activity->getRequireGEFLITST()) {
+            $this->getEmailService()->sendEmail('activity_creation_require_GEFLITST', 'email/activity_created_require_GEFLITST',
+                'Er is een fotograaf nodig voor een nieuwe GEWIS activiteit | A GEWIS activity needs a photographer of GEFLITST',
+                ['activity' => $activity]);
+
+
+        }
 
         return $activity;
     }
