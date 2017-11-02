1772fb466d37a2662d688c33766238f2d5a0ea3e
Justin Szanto <jszanto@users.noreply.github.com>
2017-11-02 11:15:48 +0100

    More compatibility fix

--- module/User/config/module.config.php
+++ module/User/config/module.config.php
@@ -145,7 +145,7 @@ return [
             'User\Controller\ApiAdmin' => 'User\Controller\ApiAdminController',
         ],
         'factories' => [
-            ApiAuthenticationController::class => ApiAuthenticationControllerFactory::class,
+            'ApiAuthenticationController' => 'ApiAuthenticationControllerFactory',
         ]
     ],
     'view_manager' => [
