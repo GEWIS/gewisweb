5667a00d881b716d80ef491b8a42627b039cde8a
Wessel Sinnema <wesselsinnema1@me.com>
2017-12-13 22:34:18 +0100

    Removed debug code comments

--- module/Company/src/Company/Form/CategoryFieldset.php
+++ module/Company/src/Company/Form/CategoryFieldset.php
@@ -77,22 +77,4 @@ class CategoryFieldset extends Fieldset
         $jc->setLanguage($lang);
         $this->setObject($jc);
     }
-
-    //protected $languages;
-
-    //// The idea of overriding this function is that we do not have access to the name in the constructor,
-    //// and the constructor is called by zf2 code. This is the first point in zf2 where we have access to the name.
-    //// Possibly not the best way to do this, but it works
-    //public function setName($name)
-    //{
-        //if (!in_array($name, $languages) && is_int($name) && $name >= 0 && $name <= count($languages)) {
-            //// In this case, it is possible to use the language code instead of the index
-            //$newName = $languages[$name];
-            //parent::setName($newName);
-            //$this->getHydrator()->setObject($newName);
-            //return $this;
-        //}
-        //parent::setName($name);
-        //$this->getHydrator()->setObject($name);
-    //}
 }
