3728c2dd6729833b5256a14d6c8556243599a4e3
Geert Derks <g.a.f.derks@student.tue.nl>
2017-12-14 21:03:16 +0100

    Removed inherited properties and methods from VirtualAlbum.

--- module/Photo/src/Photo/Model/VirtualAlbum.php
+++ module/Photo/src/Photo/Model/VirtualAlbum.php
@@ -8,47 +8,6 @@ namespace Photo\Model;
  */
 class VirtualAlbum extends Album
 {
-    /**
-     * Album ID.
-     *
-     */
-    protected $id;
-    
-    /**
-     * First date of photos in album
-     */
-    protected $startDateTime = null;
-    
-    /**
-     * End date of photos in album
-     */
-    protected $endDateTime = null;
-    
-    /**
-     * Name of the album.
-     */
-    protected $name;
-    
-    /**
-     * Parent album, null if there is no parent album.
-     */
-    protected $parent;
-    
-    /**
-     * all the subalbums
-     */
-    protected $children;
-    
-    /**
-     * all the photo's in this album.
-     */
-    protected $photos;
-    
-    /**
-     * The cover photo to display with the album.
-     */
-    protected $coverPath;
-    
     public function __construct($id)
     {
         parent::__construct();
@@ -164,96 +123,6 @@ class VirtualAlbum extends Album
     }
     
     /**
-     * Get the ID.
-     *
-     * @return int
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-    
-    /**
-     * Get the start date.
-     *
-     * @return \DateTime
-     */
-    public function getStartDateTime()
-    {
-        return $this->startDateTime;
-    }
-    
-    /**
-     * Set the start date.
-     *
-     * @param \DateTime $startDateTime
-     */
-    public function setStartDateTime(\DateTime $startDateTime)
-    {
-        $this->startDateTime = $startDateTime;
-    }
-    
-    /**
-     * Get the end date.
-     *
-     * @return \DateTime
-     */
-    public function getEndDateTime()
-    {
-        return $this->endDateTime;
-    }
-    
-    /**
-     * Set the end date.
-     *
-     * @param \DateTime $endDateTime
-     */
-    public function setEndDateTime(\DateTime $endDateTime)
-    {
-        $this->endDateTime = $endDateTime;
-    }
-    
-    /**
-     * Get the album name.
-     *
-     * @return string $name
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-    
-    /**
-     * Set the name of the album.
-     *
-     * @param string $name
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-    }
-    
-    /**
-     * Get the album cover
-     *
-     * @return string
-     */
-    public function getCoverPath()
-    {
-        return "";
-    }
-    
-    /**
-     * Set the cover photo for the album
-     *
-     * @param string $photo
-     */
-    public function setCoverPath($photo)
-    {
-        $this->coverPath = $photo;
-    }
-    
-    /**
      * Get the amount of photos in the album
      *
      * @return integer
@@ -261,7 +130,6 @@ class VirtualAlbum extends Album
     public function getPhotoCount($includeSubAlbums = false)
     {
         $count = $this->photos->count();
-        
         return $count;
     }
     
