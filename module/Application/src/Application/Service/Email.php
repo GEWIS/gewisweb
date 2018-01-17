2d925786c607ec1f22ba1fe7bfd981425be19e9e
Kevin Jilissen <kevinjilissen@hotmail.com>
2018-01-17 12:54:17 +0100

    Implemented Email service with reply-to headers

--- module/Application/src/Application/Service/Email.php
+++ module/Application/src/Application/Service/Email.php
@@ -4,10 +4,12 @@ namespace Application\Service;
 
 use Application\Service\AbstractService;
 
+use Decision\Model\Organ;
 use User\Model\NewUser as NewUserModel;
 
 use Decision\Model\Member as MemberModel;
 
+use User\Model\User;
 use Zend\Mail\Message;
 use Zend\View\Model\ViewModel;
 use Activity\Model\Activity as ActivityModel;
@@ -31,6 +33,72 @@ class Email extends AbstractService
      */
     public function sendEmail($type, $view, $subject, $data)
     {
+        $message = $this->createMessageFromView($view, $data);
+
+        $config = $this->getConfig();
+
+        $message->addFrom($config['from']);
+        $message->addTo($config['to'][$type]);
+        $message->setSubject($subject);
+
+        $this->getTransport()->send($message);
+    }
+
+    /**
+     * Send an email as a given user. The user will be added as reply-to header to the email.
+     *
+     * @param $type String Type that this email belongs to. A key in the config file for email.
+     * @param $view String Template of the email
+     * @param $subject String Subject of the email
+     * @param $data array Variables that you want to have available in the template.
+     * @param $user User The user as which the email should be sent.
+     */
+    public function sendEmailAsUser($type, $view, $subject, $data, $user)
+    {
+        $message = $this->createMessageFromView($view, $data);
+
+        $config = $this->getConfig();
+
+        $message->addFrom($config['from']);
+        $message->addTo($config['to'][$type]);
+        $message->setSubject($subject);
+        $message->setReplyTo($user->getEmail());
+
+        $this->getTransport()->send($message);
+    }
+
+    /**
+     * Send an email as a given user. The user will be added as reply-to header to the email.
+     *
+     * @param $type String Type that this email belongs to. A key in the config file for email.
+     * @param $view String Template of the email
+     * @param $subject String Subject of the email
+     * @param $data array Variables that you want to have available in the template.
+     * @param $organ Organ The user as which the email should be sent.
+     */
+    public function sendEmailAsOrgan($type, $view, $subject, $data, $organ)
+    {
+        $message = $this->createMessageFromView($view, $data);
+
+        $config = $this->getConfig();
+
+        $message->addFrom($config['from']);
+        $message->addTo($config['to'][$type]);
+        $message->setSubject($subject);
+        $message->setReplyTo($organ->getApprovedOrganInformation()->getEmail());
+
+        $this->getTransport()->send($message);
+    }
+
+    /**
+     * Constructs the Message instance for a given view with given variables.
+     *
+     * @param $view String Template of the email
+     * @param $data array Variables that you want to have available in the template.
+     * @return Message The constructed instance containing the given view as HTML body.
+     */
+    private function createMessageFromView($view, $data)
+    {
         $body = $this->render($view, $data);
 
         $html = new MimePart($body);
@@ -40,15 +108,9 @@ class Email extends AbstractService
         $mimeMessage->setParts([$html]);
 
         $message = new Message();
-
-        $config = $this->getConfig();
-
-        $message->addFrom($config['from']);
-        $message->addTo($config['to'][$type]);
-        $message->setSubject($subject);
         $message->setBody($mimeMessage);
 
-        $this->getTransport()->send($message);
+        return $message;
     }
 
 
