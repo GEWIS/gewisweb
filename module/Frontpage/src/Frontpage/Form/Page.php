GEWIS/gewisweb

commit c4d4bf78c20089e05971eaa3db33f1a07e62666d

Changed length validators for polls

module/Frontpage/src/Frontpage/Form/Page.php
@@ -110,7 +110,7 @@ class Page extends Form implements InputFilterProviderInterface
                     [
                         'name' => 'string_length',
                         'options' => [
-                            'min' => 2,
+                            'min' => 3,
                             'max' => 25
                         ]
                     ],
@@ -145,7 +145,7 @@ class Page extends Form implements InputFilterProviderInterface
                         'name' => 'string_length',
                         'options' => [
                             'min' => 3,
-                            'max' => 75
+                            'max' => 64
                         ]
                     ],
                 ],
@@ -158,7 +158,7 @@ class Page extends Form implements InputFilterProviderInterface
                         'name' => 'string_length',
                         'options' => [
                             'min' => 3,
-                            'max' => 75
+                            'max' => 64
                         ]
                     ],
                 ],

module/Frontpage/src/Frontpage/Form/Poll.php
@@ -67,8 +67,8 @@ class Poll extends Form implements InputFilterProviderInterface
                     [
                         'name' => 'string_length',
                         'options' => [
-                            'min' => 3,
-                            'max' => 75
+                            'min' => 5,
+                            'max' => 128
                         ]
                     ],
                 ],
@@ -79,8 +79,8 @@ class Poll extends Form implements InputFilterProviderInterface
                     [
                         'name' => 'string_length',
                         'options' => [
-                            'min' => 3,
-                            'max' => 75
+                            'min' => 5,
+                            'max' => 128
                         ]
                     ],
                 ],

module/Frontpage/src/Frontpage/Form/PollComment.php
@@ -55,8 +55,8 @@ class PollComment extends Form implements InputFilterProviderInterface
                     [
                         'name' => 'string_length',
                         'options' => [
-                            'min' => 3,
-                            'max' => 128
+                            'min' => 2,
+                            'max' => 32
                         ]
                     ]
                 ]

module/Frontpage/src/Frontpage/Form/PollOption.php
@@ -47,8 +47,8 @@ class PollOption extends Fieldset implements InputFilterProviderInterface
                     [
                         'name' => 'string_length',
                         'options' => [
-                            'min' => 3,
-                            'max' => 75
+                            'min' => 2,
+                            'max' => 128
                         ]
                     ],
                 ],
@@ -59,8 +59,8 @@ class PollOption extends Fieldset implements InputFilterProviderInterface
                     [
                         'name' => 'string_length',
                         'options' => [
-                            'min' => 3,
-                            'max' => 75
+                            'min' => 2,
+                            'max' => 128
                         ]
                     ],
                 ],

commit f8cc747944e166061cc5dbc113776fbeedfaa5f7

Change page form validator length back to 2, as otherwise /av is not editable. Caused by c4d4bf78c20089e05971eaa3db33f1a07e62666d.

module/Frontpage/src/Frontpage/Form/Page.php
@@ -3,7 +3,6 @@
 namespace Frontpage\Form;
 
 use Zend\Form\Form;
-use Zend\InputFilter\InputFilter;
 use Zend\InputFilter\InputFilterProviderInterface;
 use Zend\I18n\Translator\TranslatorInterface as Translator;
 
@@ -110,7 +109,7 @@ class Page extends Form implements InputFilterProviderInterface
                     [
                         'name' => 'string_length',
                         'options' => [
-                            'min' => 3,
+                            'min' => 2,
                             'max' => 25
                         ]
                     ],