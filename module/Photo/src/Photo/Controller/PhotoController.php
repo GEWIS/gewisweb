8c6098976626129314423f2300b00b11794bad22
Justin <jszanto@users.noreply.github.com>
2019-05-06 00:31:59 +0200

    Add glide for dynamic image sizing

--- module/Photo/src/Photo/Controller/PhotoController.php
+++ module/Photo/src/Photo/Controller/PhotoController.php
@@ -105,7 +105,12 @@ class PhotoController extends AbstractActionController
     {
         $photoId = $this->params()->fromRoute('photo_id');
 
-        return $this->getPhotoService()->getPhotoDownload($photoId);
+        $options = [
+            'w' => $this->params()->fromQuery('w'),
+            'h' => $this->params()->fromQuery('h')
+        ];
+        return $this->getPhotoService()->getPhotoDownload($photoId, $options);
+
     }
 
     /**
