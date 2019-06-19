07a3092ba8fa691839c5fd299deeb8e43793a14e
Koen1999 <koen.teuwen@gmail.com>
2019-06-19 11:23:26 +0200

    Small codeclimate improvement

--- module/Activity/src/Activity/Controller/ActivityCalendarController.php
+++ module/Activity/src/Activity/Controller/ActivityCalendarController.php
@@ -19,7 +19,7 @@ class ActivityCalendarController extends AbstractActionController
             'APIKey'          => $config['google_api_key'],
             'calendarKey'     => $config['google_calendar_key'],
             'success'         => $this->getRequest()->getQuery('success', false),
-            'canCreate'         => $service->canCreateProposal()
+            'canCreate'       => $service->canCreateProposal()
         ]);
     }
 
