2de684226c2b43c254426fccc6a74e8f2c9e6409
Koen <koen.teuwen@gmail.com>
2019-11-12 18:48:11 +0100

    Fixed bug in GEFLITS request

--- module/Activity/src/Activity/Service/Activity.php
+++ module/Activity/src/Activity/Service/Activity.php
@@ -109,7 +109,7 @@ class Activity extends AbstractAclService implements ServiceManagerAwareInterfac
             $activityTitle = $activity->getName(); // Fallback on Dutch title
         }
 
-        $activityTime = $activity->getBeginTime();
+        $activityTime = $activity->getBeginTime()->format('d-m-Y H:i');
 
         $type = 'activity_creation_require_GEFLITST';
         $view = 'email/activity_created_require_GEFLITST';
