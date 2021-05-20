62c0e70b99dc24de858c1c45223cc6be778a8964
Pim Rietjens <p.g.e.rietjens@student.tue.nl>
2021-05-20 16:24:49 +0200

    Intermediate change
    Small session stuff changes, all are commented out

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -6,6 +6,7 @@
  * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
+use Zend\Authentication\AuthenticationService;
 
 return [
     'router' => [
