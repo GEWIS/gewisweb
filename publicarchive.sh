896295ab21df7a82b6eadac0ed828ce436ade4a4
Tom Udding <tomudding@users.noreply.github.com>
2021-07-04 23:12:41 +0200

    Make SSH environment variables available to the non-interactive publicarchive cronjob.
    `ENV` and `BASH_ENV` did not work in the crontab. Hence we source the
    Docker environment variables (only `SSH_USERNAME` and `SSH_PASSWORD`)
    directly in `/code/publicarchive.sh`.
    
    Copying has to be done in `docker-entrypoint.sh` as it is otherwise
    not possible to build the images.

--- publicarchive.sh
+++ publicarchive.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+. /code/config/ssh.env
 rsync -aWPu --delete --chown www-data:www-data --rsh="/usr/bin/sshpass -p ${SSH_PASSWORD} ssh -o StrictHostKeyChecking=no -l ${SSH_USERNAME}" files.gewis.nl:/home/public/* /code/public/publicarchive
