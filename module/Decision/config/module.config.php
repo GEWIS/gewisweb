b2a5653911f4c71dcaecb9dfce2dcfb8ee4169c3
Koen1999 <koen.teuwen@gmail.com>
2021-07-09 14:33:31 +0200

    Removed legacy code for a data migration

--- module/Decision/config/module.config.php
+++ module/Decision/config/module.config.php
@@ -387,20 +387,5 @@ return [
                 ]
             ]
         ]
-    ],
-    'console' => [
-        'router' => [
-            'routes' => [
-                'sort-documents-legacy' => [
-                    'options' => [
-                        'route' => 'documents sort-legacy',
-                        'defaults' => [
-                            'controller' => 'Decision\Controller\Admin',
-                            'action' => 'sortDocumentsLegacy'
-                        ]
-                    ]
-                ]
-            ]
-        ]
     ]
 ];
