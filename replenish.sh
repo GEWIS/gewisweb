af91c86a094c49983a433f612e9b943366c797f8
Koen1999 <koen.teuwen@gmail.com>
2021-07-14 20:37:54 +0200

    Fixed replenish script

--- replenish.sh
+++ replenish.sh
@@ -4,13 +4,13 @@ export BRANCH="master"
 cd /tmp
 apt-get update && apt-get install -y wget
 wget --no-cache "https://github.com/${REPOSITORY}/archive/refs/heads/${BRANCH}.zip"
-unzip master.zip
-rm master.zip
-cp -R -u gewisweb-master/public/* /code/public/
+unzip "${BRANCH}.zip"
+rm "${BRANCH}.zip"
+cp -R -u gewisweb-${BRANCH}/public/* /code/public/
 chown -R  www-data:www-data /code/public
-cp -R -u gewisweb-master/data/* /code/data/
+cp -R -u gewisweb-${BRANCH}/data/* /code/data/
 chown -R  www-data:www-data /code/data
-rm -R /tmp/gewisweb-master
+rm -R /tmp/gewisweb-${BRANCH}
 cd /code
 php composer.phar dump-autoload -o --no-dev
 ./vendor/doctrine/doctrine-module/bin/doctrine-module orm:generate-proxies
