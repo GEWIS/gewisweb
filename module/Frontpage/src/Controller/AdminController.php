709051453560d4b0161508f1921f2d05f19c5fa7
Tom Udding <tomudding@users.noreply.github.com>
2021-07-14 23:41:03 +0200

    Fix admin overview This controller is actively used to display the admin overview page and can therefore not be removed (nor should it return something).

--- module/Frontpage/src/Controller/AdminController.php
+++ module/Frontpage/src/Controller/AdminController.php
@@ -5,12 +5,9 @@ namespace Frontpage\Controller;
 use Laminas\Mvc\Controller\AbstractActionController;
 use Laminas\View\View;
 
-// TODO: I believe this class is not used anywhere. Is it safe to remove?
-
 class AdminController extends AbstractActionController
 {
     public function indexAction()
     {
-        return new View();
     }
 }
