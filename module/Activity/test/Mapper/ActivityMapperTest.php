1536b73bb39854681995b99a0f9b29c112178b6e
Koen1999 <koen.teuwen@gmail.com>
2021-12-26 21:33:02 +0100

    Improved mapper tests

--- module/Activity/test/Mapper/ActivityMapperTest.php
+++ module/Activity/test/Mapper/ActivityMapperTest.php
@@ -2,13 +2,44 @@
 
 namespace ActivityTest\Mapper;
 
+use Activity\Mapper\Activity as ActivityMapper;
+use Activity\Model\Activity;
+use Application\Mapper\BaseMapper;
 use ApplicationTest\Mapper\BaseMapperTest;
+use Decision\Model\Member;
+use User\Model\User;
 
 class ActivityMapperTest extends BaseMapperTest
 {
+    /**
+     * @var ActivityMapper
+     */
+    protected BaseMapper $mapper;
+    protected User $user;
+
     public function setUp(): void
     {
         parent::setUp();
         $this->mapper = $this->serviceManager->get('activity_mapper_activity');
+        $this->object = new Activity();
+
+        $this->setUpUser();
+    }
+
+    protected function setUpUser(): void
+    {
+        $this->user = new User();
+        $this->user->setLidnr(8000);
+
+        $this->member = new Member();
+        $this->member->setLidnr(8000);
+
+        $this->user->setMember($this->member);
+    }
+
+    public function testGetUpcomingActivitiesForMember(): void
+    {
+        $this->mapper->getUpcomingActivitiesForMember($this->user);
+        $this->expectNotToPerformAssertions();
     }
 }
--- module/Application/test/Mapper/BaseMapperTest.php
+++ module/Application/test/Mapper/BaseMapperTest.php
@@ -3,21 +3,81 @@
 namespace ApplicationTest\Mapper;
 
 use Application\Mapper\BaseMapper;
-use ConsoleRunner;
+use Doctrine\ORM\EntityManager;
 use Laminas\Mvc\Application;
+use Laminas\Mvc\Service\ServiceManagerConfig;
 use Laminas\ServiceManager\ServiceManager;
 use PHPUnit\Framework\TestCase;
 
 abstract class BaseMapperTest extends TestCase
 {
-    protected Application $application;
+    protected array $applicationConfig;
+    protected ?Application $application = null;
     protected ServiceManager $serviceManager;
     protected BaseMapper $mapper;
+    protected object $object;
+    protected EntityManager $entityManager;
 
     public function setUp(): void
     {
-        $this->application = ConsoleRunner::getApplication();
-        $this->serviceManager = $this->application->getServiceManager();
+        $this->applicationConfig = include './config/application.config.php';
+        $this->getApplication();
+    }
+
+    public function getApplication(): Application
+    {
+        if ($this->application) {
+            return $this->application;
+        }
+
+        $appConfig = $this->applicationConfig;
+
+        $this->serviceManager = $this->initServiceManager($appConfig);
+
+        $this->serviceManager->setAllowOverride(true);
+        $this->setUpMockedServices();
+        $this->serviceManager->setAllowOverride(false);
+
+        $this->application = $this->bootstrapApplication($this->serviceManager, $appConfig);
+
+        $events = $this->application->getEventManager();
+        $this->application->getServiceManager()->get('SendResponseListener')->detach($events);
+
+        return $this->application;
+    }
+
+    /**
+     * Variation of {@link Application::init} but without initial bootstrapping.
+     */
+    private static function initServiceManager($configuration = []): ServiceManager
+    {
+        // Prepare the service manager
+        $smConfig = $configuration['service_manager'] ?? [];
+        $smConfig = new ServiceManagerConfig($smConfig);
+
+        $serviceManager = new ServiceManager();
+        $smConfig->configureServiceManager($serviceManager);
+        $serviceManager->setService('ApplicationConfig', $configuration);
+
+        // Load modules
+        $serviceManager->get('ModuleManager')->loadModules();
+
+        return $serviceManager;
+    }
+
+    protected function setUpMockedServices()
+    {
+    }
+
+    private function bootstrapApplication($serviceManager, $configuration = []): Application
+    {
+        // Prepare list of listeners to bootstrap
+        $listenersFromAppConfig = $configuration['listeners'] ?? [];
+        $config = $serviceManager->get('config');
+        $listenersFromConfigService = $config['listeners'] ?? [];
+
+        $listeners = array_unique(array_merge($listenersFromConfigService, $listenersFromAppConfig));
+        return $serviceManager->get('Application')->bootstrap($listeners);
     }
 
     public function testGetEntityManager(): void
@@ -29,6 +89,12 @@ abstract class BaseMapperTest extends TestCase
     public function testFindBy(): void
     {
         $this->mapper->findBy([]);
+        $this->expectNotToPerformAssertions();
+    }
+
+    public function testFindOneBy(): void
+    {
+        $this->mapper->findOneBy([]);
         $this->expectNotToPerformAssertions();
     }
 
@@ -55,4 +121,40 @@ abstract class BaseMapperTest extends TestCase
         $this->mapper->findAll();
         $this->expectNotToPerformAssertions();
     }
+
+//    public function testPersist(): void
+//    {
+//        $this->mapper->persist($this->object);
+//        $this->expectNotToPerformAssertions();
+//    }
+//
+//    public function testPersistMultiple(): void
+//    {
+//        $this->mapper->persistMultiple([$this->object]);
+//        $this->expectNotToPerformAssertions();
+//    }
+//
+//    public function testRemove(): void
+//    {
+//        $this->mapper->remove($this->object);
+//        $this->expectNotToPerformAssertions();
+//    }
+//
+//    public function testRemoveMultiple(): void
+//    {
+//        $this->mapper->removeMultiple([$this->object]);
+//        $this->expectNotToPerformAssertions();
+//    }
+//
+//    public function testRemoveById(): void
+//    {
+//        $this->mapper->removeById(0);
+//        $this->expectNotToPerformAssertions();
+//    }
+//
+//    public function testDetach(): void
+//    {
+//        $this->mapper->detach($this->object);
+//        $this->expectNotToPerformAssertions();
+//    }
 }
