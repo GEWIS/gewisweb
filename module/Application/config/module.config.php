2b61f9d06be983fd12c9fbd849d1903fed0e9c9f
Tom Udding <tomudding@users.noreply.github.com>
2022-02-03 20:34:07 +0100

    Relocate template for 418 I'm a teapot error

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -98,7 +98,7 @@ return [
         'template_map' => [
             'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
             'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
-            'application/index/teapot' => __DIR__ . '/../view/application/index/418.phtml',
+            'application/index/teapot' => __DIR__ . '/../view/error/418.phtml',
             'error/404' => __DIR__ . '/../view/error/404.phtml',
             'error/403' => __DIR__ . '/../view/error/403.phtml',
             'error/418' => __DIR__ . '/../view/error/418.phtml',
