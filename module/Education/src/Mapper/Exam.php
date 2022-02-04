0d90c57dd9a3ad2e6bda9625d5a3fd220d398d9a
Tom Udding <tomudding@users.noreply.github.com>
2022-02-04 13:00:08 +0100

    Replace deprecated `transactional` with `wrapInTransaction`

--- module/Education/src/Mapper/Exam.php
+++ module/Education/src/Mapper/Exam.php
@@ -12,25 +12,6 @@ use Education\Model\Exam as ExamModel;
 class Exam extends BaseMapper
 {
     /**
-     * Transactional Doctrine wrapper.
-     *
-     * Instead of the EntityManager, this inserts this Mapper into the
-     * function.
-     *
-     * @param Closure $func
-     *
-     * @return mixed
-     */
-    public function transactional(Closure $func): mixed
-    {
-        return $this->em->transactional(
-            function ($em) use ($func) {
-                return $func($this);
-            }
-        );
-    }
-
-    /**
      * @inheritDoc
      */
     protected function getRepositoryName(): string
