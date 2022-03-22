5de8391c1f41902f40dfd25c42c48c9eaea2d323
Tom Udding <tomudding@users.noreply.github.com>
2022-03-22 11:48:34 +0100

    Actually log the fatal error and set status code for Zabbix
    To make sure that we can actually see what is going on, we must
    not forget to actually log the error somewhere. It will be logged
    to `php.log`.
    
    Additionally, by using `readfile` the response status code is
    automatically 200. This breaks the uptime reporting provided to
    us by the CBC. Hence, we force the status code to be 500.

--- public/index.php
+++ public/index.php
@@ -24,6 +24,11 @@ try {
 } catch (Throwable $e) {
     // Only show the global exception page if we are not in development mode.
     if ('development' !== APP_ENV) {
+        // Make sure that we actually log the problem.
+        error_log('Fatal ' . $e);
+
+        // Output the exception page (and force status code to 500, otherwise it will be 200).
+        header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error');
         readfile('/code/public/errors/exception.html');
     }
 }
