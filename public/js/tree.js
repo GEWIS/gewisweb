69e4288f75c19ac80dc4243074653deccfb1a911
Tom Udding <tomudding@users.noreply.github.com>
2022-06-07 16:20:11 +0200

    Remove duplicate tree navigation code
    This also changes the FontAwesome icons from the generic plus/minus
    to the folder icons.

--- public/js/tree.js
+++ public/js/tree.js
@@ -2,25 +2,23 @@ $.fn.extend({
     treed: function () {
         //initialize each of the top levels
         var tree = $(this);
-        tree.addClass("tree");
-        tree.find('li').has("ul").each(function () {
-            var branch = $(this); //li with children ul
-            branch.prepend("<i class='fas fa-plus-circle toggleicon'></i>");
+        tree.addClass('tree');
+        tree.find('li').has('ul').each(function () {
+            let branch = $(this); //li with children ul
+            branch.prepend('<i class="fas fa-folder-plus indicator"></i>');
             branch.addClass('branch');
             branch.on('click', function (e) {
                 if (this == e.target) {
-                    var icon = $(this).children('i:first');
-                      icon.toggleClass("fa-plus-circle fa-minus-circle");
+                    let icon = $(this).children('i:first');
+                    icon.toggleClass('fa-folder-plus fa-folder-minus');
                     $(this).children().children().toggle();
                 }
             });
             branch.children().children().toggle();
         });
         //fire event from the dynamically added icon
-        $('.branch .toggleicon').on('click', function () {
+        $('.branch .indicator').on('click', function () {
             $(this).closest('li').click();
         });
-        //fire event to open branch if the li contains an anchor instead of text
-
     }
 });
