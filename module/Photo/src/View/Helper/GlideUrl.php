7a4c99498ccd0f831d836920d6bbcbfe01162c45
Tom Udding <tomudding@users.noreply.github.com>
2022-07-28 18:21:56 +0200

    Re-enable WEBP encoding for Glide

--- module/Photo/src/View/Helper/GlideUrl.php
+++ module/Photo/src/View/Helper/GlideUrl.php
@@ -37,10 +37,9 @@ class GlideUrl extends AbstractHelper
         array $params,
     ): string {
         // If the encoding format is not specifically defined, default to webp.
-        // TODO: Temporarily disabled.
-        // if (!isset($params['fm'])) {
-        //     $params['fm'] = 'webp';
-        // }
+         if (!isset($params['fm'])) {
+             $params['fm'] = 'webp';
+         }
 
         return $this->urlBuilder->getUrl($imagePath, $params);
     }
