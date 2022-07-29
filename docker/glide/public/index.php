3739eb84ff68fa10f7047c255c9253b4da2f3272
Tom Udding <tomudding@users.noreply.github.com>
2022-07-29 14:58:48 +0200

    Override Glide `Server` to use slightly less RAM

--- docker/glide/public/index.php
+++ docker/glide/public/index.php
@@ -8,11 +8,127 @@ chdir(dirname(__DIR__));
 
 require 'vendor/autoload.php';
 
-use League\Glide\Signatures\SignatureFactory;
-use League\Glide\Signatures\SignatureException;
+use League\Flysystem\FilesystemException as FilesystemV2Exception;
+use League\Glide\{
+    Server,
+    ServerFactory,
+};
+use League\Glide\Filesystem\{
+    FileNotFoundException,
+    FilesystemException,
+};
+use League\Glide\Signatures\{
+    SignatureFactory,
+    SignatureException,
+};
+
+
+/**
+ * Override the actual `ServerFactory`, such that our custom `Server` is returned.
+ */
+class GlideServerFactory extends ServerFactory
+{
+    /**
+     * Get configured custom server.
+     *
+     * @return GlideServer Configured Glide server.
+     */
+    public function getServer(): GlideServer
+    {
+        $server = new GlideServer(
+            $this->getSource(),
+            $this->getCache(),
+            $this->getApi()
+        );
+
+        $server->setSourcePathPrefix($this->getSourcePathPrefix() ?: '');
+        $server->setCachePathPrefix($this->getCachePathPrefix() ?: '');
+        $server->setGroupCacheInFolders($this->getGroupCacheInFolders());
+        $server->setCacheWithFileExtensions($this->getCacheWithFileExtensions());
+        $server->setDefaults($this->getDefaults());
+        $server->setPresets($this->getPresets());
+        $server->setBaseUrl($this->getBaseUrl() ?: '');
+        $server->setResponseFactory($this->getResponseFactory());
+
+        if ($this->getTempDir()) {
+            $server->setTempDir($this->getTempDir());
+        }
+
+        return $server;
+    }
+}
+
+/**
+ * Override the actual `Server`, such that creation of images does not use the memory hogging
+ * `file_{get,put}_contents()` functions.
+ */
+class GlideServer extends Server
+{
+    /**
+     * Generate manipulated image.
+     *
+     * @param string $path   Image path.
+     * @param array  $params Image manipulation params.
+     *
+     * @return string Cache path.
+     *
+     * @throws FileNotFoundException
+     * @throws FilesystemException
+     */
+    public function makeImage($path, array $params): string
+    {
+        $sourcePath = $this->getSourcePath($path);
+        $cachedPath = $this->getCachePath($path, $params);
+
+        if (true === $this->cacheFileExists($path, $params)) {
+            return $cachedPath;
+        }
+
+        if (false === $this->sourceFileExists($path)) {
+            throw new FileNotFoundException('Could not find the image `'.$sourcePath.'`.');
+        }
+
+        try {
+            // MODIFIED: Do not read the whole file into memory (`file_get_contents()`), simply check its existence.
+            $source = $this->source->fileExists(
+                $sourcePath
+            );
+        } catch (FilesystemV2Exception $exception) {
+            throw new FilesystemException('Could not read the image `'.$sourcePath.'`.');
+        }
+
+        // We need to write the image to the local disk before
+        // doing any manipulations. This is because EXIF data
+        // can only be read from an actual file.
+        $tmp = tempnam($this->tempDir, 'Glide');
+
+        // MODIFIED: Copy the file with a native copy instead of using `file_put_contents()`;
+        try {
+            $this->source->copy(
+                $sourcePath,
+                $tmp,
+            );
+        } catch (FilesystemV2Exception $exception) {
+            throw new FilesystemException('Could not write temp file for `'.$sourcePath.'`.');
+        }
+
+        try {
+            $this->cache->write(
+                $cachedPath,
+                $this->api->run($tmp, $this->getAllParams($params))
+            );
+        } catch (FilesystemV2Exception $exception) {
+            throw new FilesystemException('Could not write the image `'.$cachedPath.'`.');
+        } finally {
+            unlink($tmp);
+        }
+
+        return $cachedPath;
+    }
+}
 
 // Setup Glide server
-$server = League\Glide\ServerFactory::create([
+$server = GlideServerFactory::create([
     'source' => '/code/public/data',
     'cache' => '/glide/cache',
     'driver' => 'gd',
