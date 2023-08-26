196a19f335ce94689ac47ddbf75a44abfd3a2983
Tom Udding <tomudding@users.noreply.github.com>
2023-08-26 18:25:55 +0200

    Add better error handling and earlier `exit()`s to GEWISDB sync
    Changes important string concatenations to `sprintf` to make it
    easier to see what actually happens. To enable this the `catch`
    blocks haveto be able to catch `Error`s (internal PHP errors)
    next to `Exception`s.
    
    This also changes the DSNs to take the `port`s and `charset`s
    of the databases into account and moves `setAttribute()` to the
    constructor. Technically, `PDO::ATTR_ERRMODE` defaults to
    `PDO::ERRMODE_EXCEPTION`, however, being setting it explicitly
    should make it more obvious.
    
    Added `ROLLBACK` statement if a top-level error occurs, inserts
    and removals are caught separately. Moved the restore of the
    foreign key constraints to a `finally` block to ensure it is
    always executed (as long as the connection is still working).

--- importdb.php
+++ importdb.php
@@ -1,135 +1,236 @@
 <?php
+
+declare(strict_types=1);
+
 /**
- * This is a separate script that copies the GEWIS Report Database to the Web
- * database.
+ * This is a separate script that copies the GEWIS Report Database to the website database.
  *
  * It is a simple PostgreSQL to MySQL copy script.
  */
 
-echo "Commencing sync with gewisdb\n";
+echo 'Commencing sync with GEWISDB...' . PHP_EOL;
 
 try {
-// connections
+    // Setting up connections.
     $config = include 'config/autoload/gewisdb.local.php';
 
-    $pgconn = new PDO('pgsql:host=' . $config['host'] . ';dbname=' . $config['dbname']
-        . ';user=' . $config['user'] . ';password=' . $config['password']);
-    $pgconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pgconn = new PDO(
+        sprintf(
+            'pgsql:host=%s;port=%d;dbname=%s;options=\'--client_encoding=%s\'',
+            $config['host'],
+            $config['port'],
+            $config['dbname'],
+            $config['charset']
+        ),
+        $config['user'],
+        $config['password'],
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        ],
+    );
 
     $doctrineConf = include 'config/autoload/doctrine.local.php';
     $params = $doctrineConf['doctrine']['connection']['orm_default']['params'];
 
     $myconn = new PDO(
-        'mysql:host=' . $params['host'] . ';dbname=' . $params['dbname'] . ';charset=' . $params['charset'],
+        sprintf(
+            'mysql:host=%s;port=%d;dbname=%s;charset=%s',
+            $params['host'],
+            $params['port'],
+            $params['dbname'],
+            $params['charset'],
+        ),
         $params['user'],
-        $params['password']
+        $params['password'],
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES ' . $params['charset'] . ' COLLATE ' . $params['collate'],
+        ],
     );
-    $myconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception|Error $e) {
+    echo 'ERROR: Failed to connect to GEWISDB or GEWISWEB.' . PHP_EOL;
+    echo $e->getMessage() . PHP_EOL;
+    echo $e->getTraceAsString() . PHP_EOL;
+    exit(1);
+}
 
-    /* which tables to sync */
-    $tables = [
-        'Address',
-        'BoardMember',
-        'Decision',
-        'MailingList',
-        'Meeting',
-        'Member',
-        'members_mailinglists',
-        'Keyholder',
-        'Organ',
-        'OrganMember',
-        'organs_subdecisions',
-        'SubDecision'
-    ];
+/* which tables to sync */
+$tables = [
+    'Address',
+    'BoardMember',
+    'Decision',
+    'MailingList',
+    'Meeting',
+    'Member',
+    'members_mailinglists',
+    'Keyholder',
+    'Organ',
+    'OrganMember',
+    'organs_subdecisions',
+    'SubDecision'
+];
 
-    echo "Connection with gewisdb set up\n";
-    echo "Disabling foreign key constraints\n";
+echo "Connection with GEWISDB and GEWISWEB set up" . PHP_EOL;
+echo "Disabling foreign key constraints on GEWISWEB" . PHP_EOL;
 
-// to not trip up InnoDB
+try {
+    // Disabling the foreign key constraints on GEWISWEB is necessary to silence InnoDB as (mostly) members can be
+    // removed while they are still referenced elsewhere.
     $myconn->query('SET foreign_key_checks = 0');
-    $myconn->query('START TRANSACTION');
+} catch (PDOException $e) {
+    echo 'ERROR: Failed to disable foreign key constraints on GEWISWEB.' . PHP_EOL;
+    exit(1);
+}
 
-    $pks = $myconn->query("SELECT TABLE_NAME, COLUMN_NAME
-                    FROM INFORMATION_SCHEMA.key_column_usage 
-                    WHERE table_schema = '" . $params['dbname'] . "' AND CONSTRAINT_NAME = 'PRIMARY'")
-                    ->fetchAll(PDO::FETCH_COLUMN|PDO::FETCH_GROUP);
+try {
+    // Start the actual synchronisation.
+    echo 'Creating restore point for GEWISWEB...' . PHP_EOL;
+    $myconn->query('START TRANSACTION');
+    echo 'Restore point for GEWISWEB created.' . PHP_EOL;
+
+    $pksQuery = <<<'PKS'
+SELECT TABLE_NAME, COLUMN_NAME
+FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+WHERE TABLE_SCHEMA = '%s' AND CONSTRAINT_NAME = 'PRIMARY'
+PKS;
+    $pks = $myconn->query(sprintf($pksQuery, $params['dbname']))
+        ->fetchAll(PDO::FETCH_COLUMN|PDO::FETCH_GROUP);
 
     foreach ($tables as $table) {
-        $query = "SELECT * FROM $table";
-        $stmt = $pgconn->query($query);
-        echo "Table $table\n";
+        $query = sprintf('SELECT * FROM %s', $table);
+        $pgStmt = $pgconn->query($query);
+        echo 'Syncing table "' . $table . '"...' . PHP_EOL;
 
-        $insertcount = 0;
+        $insertCount = 0;
         // Insert new data
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            $fields = '(' . implode(', ', array_keys($row)) . ')';
-            $values = '(' . implode(', ', array_map(function ($a) {
-                    return ':' . $a;
-                }, array_keys($row))) . ')';
-            $updates = implode(', ', array_map(function ($a) {
-                return $a . '=VALUES(' . $a . ')';
-            }, array_keys($row)));
+        while ($pgData = $pgStmt->fetch(PDO::FETCH_ASSOC)) {
+            $fields = sprintf(
+                '(%s)',
+                implode(
+                    ', ',
+                    array_keys($pgData),
+                ),
+            );
+            $values = sprintf(
+                '(%s)',
+                implode(
+                    ', ',
+                    array_map(
+                        function ($a) {
+                            return ':' . $a;
+                        },
+                        array_keys($pgData),
+                    ),
+                ),
+            );
+            $updates = implode(
+                ', ',
+                array_map(
+                    function ($a) {
+                        return $a . '=VALUES(' . $a . ')';
+                    },
+                    array_keys($pgData),
+                ),
+            );
 
-            $data = $row;
+            $data = $pgData;
 
             // see if we can fetch about 256 more rows (gigantic speed increase)
-            for ($i = 0; $i < 256 && ($row2 = $stmt->fetch(PDO::FETCH_ASSOC)); $i++) {
-                $values .= ', (' . implode(', ', array_map(function ($a) use ($i) {
-                        return ':' . $a . $i;
-                    }, array_keys($row2))) . ')';
-                foreach ($row2 as $key => $value) {
+            for ($i = 0; $i < 256 && ($pgData2 = $pgStmt->fetch(PDO::FETCH_ASSOC)); $i++) {
+                $values .= sprintf(
+                    ', (%s)',
+                    implode(
+                        ', ',
+                        array_map(
+                            function ($a) use ($i) {
+                                return ':' . $a . $i;
+                            },
+                            array_keys($pgData2),
+                        ),
+                    ),
+                );
+
+                foreach ($pgData2 as $key => $value) {
                     $data[$key . $i] = $value;
                 }
             }
 
-            $sql = "INSERT IGNORE INTO $table $fields VALUES $values ON DUPLICATE KEY UPDATE $updates";
-            $stmtt = $myconn->prepare($sql);
+            $sql = sprintf(
+                'INSERT IGNORE INTO %s %s VALUES %s ON DUPLICATE KEY UPDATE %s',
+                $table,
+                $fields,
+                $values,
+                $updates,
+            );
+            $insertStmt = $myconn->prepare($sql);
 
             try {
-                $stmtt->execute($data);
-                $insertcount += $stmtt->rowCount();
+                $insertStmt->execute($data);
+                $insertCount += $insertStmt->rowCount();
             } catch (Exception $e) {
-                echo "ERROR: Failed to import data of table " . $table . "\n";
-                echo $e->getMessage();
-                echo "\n";
-                echo $e->getTraceAsString();
-                echo "\n\n";
+                echo 'ERROR: Failed to import data of table "' . $table . '"' . PHP_EOL;
+                echo $e->getMessage() . PHP_EOL;
+                echo $e->getTraceAsString() . PHP_EOL;
             }
 
             echo '.';
         }
-        echo PHP_EOL . "Inserted or updated " . $insertcount . " rows in $table (updates count double)" . PHP_EOL;
+
+        echo PHP_EOL . PHP_EOL;
+        echo 'Inserted or updated ' . $insertCount . ' rows in "' . $table . '" (updates count double)' . PHP_EOL;
 
         // Removing old data
-        $id_sql = "SELECT " . $pks[$table][0] . " FROM $table";
-        $ids = $pgconn->query($id_sql)->fetchAll(PDO::FETCH_COLUMN);
-        if (count($ids) === 0) $ids = [null];
-        $del_sql = "DELETE FROM $table WHERE " . $pks[$table][0] . " NOT IN (" . str_repeat('?,', count($ids) - 1) . '?' . ")";
-        $del_stmt = $myconn->prepare($del_sql);
-        try {
-            $del_stmt->execute($ids);
-            echo "Deleted " . $del_stmt->rowCount() . " rows from $table" . PHP_EOL;
-        } catch (Exception $e) {
-            echo "ERROR: Failed to remove data from table " . $table . PHP_EOL;
-            echo $e->getMessage();
-            echo "\n";
-            echo $e->getTraceAsString();
-            echo "\n\n";
+        $idSql = sprintf(
+            'SELECT %s FROM %s',
+            $pks[$table][0],
+            $table,
+        );
+        $ids = $pgconn->query($idSql)->fetchAll(PDO::FETCH_COLUMN);
+
+        if (0 === count($ids)) {
+            $ids = [null];
         }
-        
-        echo "\n\n";
+
+        $deletionSql = sprintf(
+            "DELETE FROM %s WHERE %s NOT IN (%s?)",
+            $table,
+            $pks[$table][0],
+            str_repeat('?,', count($ids) - 1),
+        );
+        $removeStmt = $myconn->prepare($deletionSql);
+
+        try {
+            $removeStmt->execute($ids);
+            echo 'Deleted ' . $removeStmt->rowCount() . ' rows from "' . $table . '"' . PHP_EOL;
+        } catch (Exception $e) {
+            echo 'ERROR: Failed to remove data from table "' . $table . '"' . PHP_EOL;
+            echo $e->getMessage() . PHP_EOL;
+            echo $e->getTraceAsString() . PHP_EOL;
+        }
+
+        echo PHP_EOL;
     }
 
-    echo "Enabling foreign key constraints\n";
-
+    echo 'Committing transaction...' . PHP_EOL;
     $myconn->query('COMMIT');
-    $myconn->query('SET foreign_key_checks = 1');
 
-    echo "Sync with gewisdb completed \n\n\n";
-} catch (Exception $e) {
-    echo "ERROR: Sync with gewisdb failed because of exception\n";
-    echo $e->getMessage();
-    echo "\n";
-    echo $e->getTraceAsString();
-    echo "\n\n\n";
+    echo 'Transaction committed.' . PHP_EOL;
+    echo 'Sync with GEWISDB completed!' . PHP_EOL;
+} catch (Exception|Error $e) {
+    echo 'ERROR: Sync with GEWISDB failed because of exception' . PHP_EOL;
+    echo $e->getMessage() . PHP_EOL;
+    echo $e->getTraceAsString() . PHP_EOL;
+
+    echo 'Restoring GEWISWEB...' . PHP_EOL;
+    try {
+        $myconn->query('ROLLBACK');
+        echo 'Restored GEWISWEB state.' . PHP_EOL;
+    } catch (PDOException) {
+        echo 'ERROR: Could not restore GEWISWEB' . PHP_EOL;
+        echo $e->getMessage() . PHP_EOL;
+        echo $e->getTraceAsString() . PHP_EOL;
+    }
+} finally {
+    echo 'Enabling foreign key constraints...' . PHP_EOL;
+    $myconn->query('SET foreign_key_checks = 1');
 }
