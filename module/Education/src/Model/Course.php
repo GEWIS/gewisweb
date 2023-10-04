0278beb9eb10e3566b3c38bf1dc1f8d4e97049c8
Jort van Driel <10116429+JortvD@users.noreply.github.com>
2023-10-04 11:16:00 +0000

    Add similar course table

--- module/Education/src/Model/Course.php
+++ module/Education/src/Model/Course.php
@@ -9,6 +9,10 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\InverseJoinColumn;
+use Doctrine\ORM\Mapping\JoinColumn;
+use Doctrine\ORM\Mapping\JoinTable;
+use Doctrine\ORM\Mapping\ManyToMany;
 use Doctrine\ORM\Mapping\OneToMany;
 use Doctrine\ORM\Mapping\OrderBy;
 use Laminas\Permissions\Acl\Resource\ResourceInterface;
@@ -44,9 +48,42 @@ class Course implements ResourceInterface
     #[OrderBy(value: ['date' => 'ASC'])]
     protected Collection $documents;
 
+    /**
+     * Courses that say they are similar to this course
+     *
+     * @var Collection<array-key, Course>
+     */
+    #[ManyToMany(
+        targetEntity: self::class,
+        mappedBy: 'similarCoursesTo',
+    )]
+    protected Collection $similarCoursesFrom;
+
+    /**
+     * Courses similar to this course
+     * 
+     * @var Collection<array-key, Course>
+     */
+    #[JoinTable(name: 'SimilarCourse')]
+    #[JoinColumn(
+        name: 'course_code', 
+        referencedColumnName: 'code',
+    )]
+    #[InverseJoinColumn(
+        name: 'similar_course_code', 
+        referencedColumnName: 'code',
+    )]
+    #[ManyToMany(
+        targetEntity: self::class, 
+        inversedBy: 'similarCoursesFrom',
+    )]
+    private Collection $similarCoursesTo;
+
     public function __construct()
     {
         $this->documents = new ArrayCollection();
+        $this->similarCoursesFrom = new ArrayCollection();
+        $this->similarCoursesTo = new ArrayCollection();
     }
 
     /**
