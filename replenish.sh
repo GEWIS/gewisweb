f65fd5db34c64a56bb738a62342c78e77a5cde64
Tom Udding <tomudding@users.noreply.github.com>
2023-10-06 19:02:46 +0200

    Move SCSS source files out of webroot
    This moves the SCSS source files out of the webroot to ensure that
    they are not accessible from the actual website. Furthermore, the
    Bootstrap and FontAwesome source files are no longer kept in version
    control. This should make it easier to perform dependency upgrades
    without creating massive changes.
    
    To facilitate this the `Dockerfile`s had to be changed. To make sure
    that the relevant files are copied just in time for compilation of
    the CSS files. This also means that we dropped
    `slowprog/composer-copy-file`, which we used to copy the files after
    a Composer install or update.
    
    Furthermore, this removes the dependency on an external request to
    be able to install Composer. The Composer binary now comes directly
    from the Composer Docker image. As such, the `composer.phar` has
    also been removed from version control.

--- replenish.sh
+++ replenish.sh
@@ -13,9 +13,9 @@ rm -R /tmp/gewisweb-${BRANCH}
 cd /code
 if [ "${APP_ENV}" == 'production' ]
 then
-    php composer.phar dump-autoload -o --no-dev
+    composer dump-autoload -o --no-dev
 else
-    php composer.phar dump-autoload -o
+    composer dump-autoload -o
 fi
 ./orm orm:generate-proxies
 rm -Rf /code/data/cache/*
