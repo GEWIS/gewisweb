f8df3aa4cfeb0c29bd8fd112c4275e7c53b6bc8e
Tom Udding <tomudding@users.noreply.github.com>
2024-12-10 08:46:48 +0100

    fix: time diff calculation
    I used `ceil` instead of `floor` for weeks to help with underestimation of the
    time till an activity (or how long an activity would still run for). However,
    this resulted in a lot of confusion as it was possible for an activity to
    "happen" in two weeks while it would have been 1 week and 1 day.
    
    As such, the formatter now displays with multiple units if possible. But only
    for years, months, weeks, and days OR hours and minutes OR minutes and seconds.
    
    --
    
    For Dutch, this also changes `jaren` to `jaar` and `uren` to `uur`.

--- module/Application/src/View/Helper/TimeDiff.php
+++ module/Application/src/View/Helper/TimeDiff.php
@@ -8,7 +8,12 @@ use DateTime;
 use Laminas\Mvc\I18n\Translator;
 use Laminas\View\Helper\AbstractHelper;
 
-use function ceil;
+use function array_pop;
+use function array_slice;
+use function count;
+use function floor;
+use function implode;
+use function sprintf;
 
 class TimeDiff extends AbstractHelper
 {
@@ -20,26 +25,62 @@ class TimeDiff extends AbstractHelper
         DateTime $start,
         DateTime $end,
     ): string {
-        $units = [
-            'year' => 31536000,
-            'month' => 2592000,
-            'week' => 604800,
-            'day' => 86400,
-            'hour' => 3600,
-            'minute' => 60,
+        $diffInterval = $start->diff($end);
+        $days = false === $diffInterval->days ? 0 : $diffInterval->days;
+
+        $diff = [
+            'year' => $diffInterval->y,
+            'month' => $diffInterval->m,
+            'week' => (int) floor($days / 7) % 4,
+            'day' => $days % 7,
+            'hour' => $diffInterval->h,
+            'minute' => $diffInterval->i,
+            'second' => $diffInterval->s,
         ];
 
-        $diff = $end->getTimestamp() - $start->getTimestamp();
-
-        foreach ($units as $unit => $value) {
-            if ($diff >= $value) {
-                $number = ceil($diff / $value);
-
-                return $number . ' ' . $this->translator->translatePlural($unit, $unit . 's', (int) $number);
+        $units = [];
+        $result = [];
+        if (0 === $days) {
+            if (
+                0 === $diff['hour']
+                && 0 === $diff['minute']
+            ) {
+                $result[] = $this->translator->translate('less than a minute');
+            } elseif (0 === $diff['hour']) {
+                $units = ['minute', 'second'];
+            } else {
+                $units = ['hour', 'minute'];
             }
+        } else {
+            $units = ['year', 'month', 'week', 'day'];
         }
 
-        return $diff . '' . $this->translator->translatePlural('second', 'seconds', $diff);
+        foreach ($units as $unit) {
+            if (0 === $diff[$unit]) {
+                continue;
+            }
+
+            $result[] = sprintf(
+                '%d %s',
+                $diff[$unit],
+                $this->translator->translatePlural($unit, $unit . 's', $diff[$unit]),
+            );
+        }
+
+        if (1 < count($result)) {
+            return sprintf(
+                '%s %s %s',
+                implode(', ', array_slice($result, 0, -1)),
+                $this->translator->translate('and'),
+                array_pop($result),
+            );
+        }
+
+        if (1 === count($result)) {
+            return $result[0];
+        }
+
+        return $this->translator->translate('unable to estimate time');
     }
 
     /**
@@ -54,5 +95,6 @@ class TimeDiff extends AbstractHelper
         $this->translator->translatePlural('day', 'days', 1);
         $this->translator->translatePlural('hour', 'hours', 1);
         $this->translator->translatePlural('minute', 'minutes', 1);
+        $this->translator->translatePlural('second', 'seconds', 1);
     }
 }
