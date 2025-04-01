0a04a6b9a5bb107b21e462f4e16cdf170eaa4d83
Tom Udding <tomudding@users.noreply.github.com>
2025-04-01 14:42:45 +0200

    fix: GEWIS console splash being fully external
    We host everything ourselves to ensure that it cannot be easily altered and we
    have no issues with GDPR (similar to why we removed the Google-hosted fonts).
    The former could have been fixed with subresource integrity hashes, but with the
    privacy argument as well it is just easier to do it like this.
    
    Similar changes have been added (or are pending) for Aurora and SudoSOS.

---
+++ public/js/console-splash.js
@@ -0,0 +1,34 @@
+/* eslint-disable */
+console.log(
+  `%c
+█████████ █████████  
+███    ▀▀ ███        
+ ███  ▄▄▄ ████████   
+ ███   ██ ███        
+  ███████ ███████    
+   ▄▄▄▄     ▄▄▄▄
+   ████  ▄  ████     
+   ▀███▄███▄███▀
+    █████▀█████      
+    ▀███▀ ▀███▀
+     ▀▀▀ ▀ ▀▀▀
+      ███████        
+       █████         
+       █████         
+        ███          
+        ▀▀▀         
+       █████        
+      ███▀▀██        
+       ▀▀█▄▄        
+      ██▄▄███        
+       █████         
+
+`,
+  'color: #d40000',
+);
+console.log(
+  "%cCurious what's behind the scenes? Check out our projects at %chttps://github.com/gewis%c!",
+  'font-size: 20px',
+  'font-size: 18px; color: #d40000;',
+  'font-size: 18px',
+);
